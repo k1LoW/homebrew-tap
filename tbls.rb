9f8ea54c2fa62676e18763277e2a09c11a2bbdee
k1LoW <k1lowxb@gmail.com>
2019-01-02 18:29:46 +0900

    Update tbls version to v1.7.1

--- tbls.rb
+++ tbls.rb
@@ -1,9 +1,9 @@
 class Tbls < Formula
   desc 'tbls is a CI-Friendly tool for document a database, written in Go.'
   homepage 'https://github.com/k1LoW/tbls'
-  version '1.7.0'
+  version '1.7.1'
   url "https://github.com/k1LoW/tbls/releases/download/v#{version}/tbls_v#{version}_darwin_amd64.zip"
-  sha256 '391ea2afb240d39020a1a2f1cded029f513883456aa28b8710ba1979c64f7e79'
+  sha256 'e85061df5656390447f14ca0a35633b25c8ef4311bcbc108f0b9f798362ba636'
 
   def install
     bin.install 'tbls'
