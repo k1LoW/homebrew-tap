16cb9d33211f8dccc98eb3e6bccd28a66ee30e54
k1LoW <k1lowxb@gmail.com>
2019-01-07 16:38:25 +0900

    Update evry version to v0.1.2

--- evry.rb
+++ evry.rb
@@ -1,9 +1,9 @@
 class Evry < Formula
   desc 'evry split STDIN stream and execute specified command every N lines/seconds.'
   homepage 'https://github.com/k1LoW/evry'
-  version '0.1.1'
+  version '0.1.2'
   url "https://github.com/k1LoW/evry/releases/download/v#{version}/evry_v#{version}_darwin_amd64.zip"
-  sha256 '0412a3d94077ad9ff287e6f80cc49e29d3805557c7719b460f045a6ff206f02f'
+  sha256 '1d927aad667ffc2b6eabab4bd9ea8724026806de31abd18043ca07db474aea56'
 
   def install
     bin.install 'evry'
