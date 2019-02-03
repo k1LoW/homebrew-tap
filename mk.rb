189cda28b009330f1366ffea81bdb197fd968582
k1LoW <k1lowxb@gmail.com>
2019-02-03 17:36:55 +0900

    Update mk version to v0.2.0

--- mk.rb
+++ mk.rb
@@ -1,9 +1,9 @@
 class Mk < Formula
   desc 'mk find Makefile by walking up parent directories and execute `make`.'
   homepage 'https://github.com/k1LoW/mk'
-  version '0.1.0'
+  version '0.2.0'
   url "https://github.com/k1LoW/mk/releases/download/v#{version}/mk_v#{version}_darwin_amd64.zip"
-  sha256 '06555b40c0d3ef342ec0db62c536a340e66db8d0f9189a772d0e702f2c5ac2c0'
+  sha256 '81801a0217938a4baa72cc0f629b03213e170d6172a7d41311c86a1c822e6eb5'
 
   def install
     bin.install 'mk'
