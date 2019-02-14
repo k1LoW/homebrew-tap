9cee8bf7b40b72c7174f5bbc8d288df48784e7b3
k1low <k1low@pepabo.com>
2019-02-15 01:07:23 +0900

    Update harvest version to v0.2.2

--- harvest.rb
+++ harvest.rb
@@ -1,9 +1,9 @@
 class Harvest < Formula
   desc 'Portable log aggregation tool for middle-scale system operation/observation.'
   homepage 'https://github.com/k1LoW/harvest'
-  version '0.2.1'
+  version '0.2.2'
   url "https://github.com/k1LoW/harvest/releases/download/v#{version}/harvest_v#{version}_darwin_amd64.zip"
-  sha256 '2e0c551d9adece6789686c734e3cc1484d27646660f96152231843f0fa9e6921'
+  sha256 '6f5f2ae6ca191f0ba99731ed59ac88e675a0c9a8757cb88b581ad22460e2d614'
 
   def install
     bin.install 'hrv'
