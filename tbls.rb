3b17d365c3db992157fac16e0a5a7284d81e6679
k1LoW <k1lowxb@gmail.com>
2019-03-13 18:58:10 +0900

    Brew formula update for tbls version v1.10.0

--- tbls.rb
+++ tbls.rb
@@ -2,9 +2,9 @@
 class Tbls < Formula
   desc "tbls is a CI-Friendly tool for document a database, written in Go."
   homepage "https://github.com/k1LoW/tbls"
-  url "https://github.com/k1LoW/tbls/releases/download/v1.9.0/tbls_v1.9.0_darwin_amd64.zip"
-  version "1.9.0"
-  sha256 "2bfa2cb047fd518804b11b28d407751bfadb1ae445fb78bccf2ebf5a61958fd0"
+  url "https://github.com/k1LoW/tbls/releases/download/v1.10.0/tbls_v1.10.0_darwin_amd64.zip"
+  version "1.10.0"
+  sha256 "20fef70998719e45d6145e091f3a8112f8e6eedc8f9b42d037bf7e5453b88adc"
 
   def install
     bin.install 'tbls'
