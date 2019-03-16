033c96db2ad040e3fdf81dbbc4c678f54c47712b
k1LoW <k1lowxb@gmail.com>
2019-03-16 22:22:04 +0900

    Brew formula update for harvest version v0.6.2

--- harvest.rb
+++ harvest.rb
@@ -2,9 +2,9 @@
 class Harvest < Formula
   desc "Portable log aggregation tool for middle-scale system operation/observation."
   homepage "https://github.com/k1LoW/harvest"
-  url "https://github.com/k1LoW/harvest/releases/download/v0.6.1/harvest_v0.6.1_darwin_amd64.zip"
-  version "0.6.1"
-  sha256 "7437f5dbfb9a4a723e280c0cbe0f041deabe3d9493a079a2049240da52af0b92"
+  url "https://github.com/k1LoW/harvest/releases/download/v0.6.2/harvest_v0.6.2_darwin_amd64.zip"
+  version "0.6.2"
+  sha256 "4a6ced318bbcd35c2c54b28a3d9e0108d336be31a47426e8cd0001550339331e"
 
   def install
     bin.install 'hrv'
