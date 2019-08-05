1cca466fdf0aaa6c3a4980128c281bde834de6bb
k1LoW <k1lowxb@gmail.com>
2019-08-05 09:11:30 +0900

    Brew formula update for metr version v0.1.1

--- metr.rb
+++ metr.rb
@@ -2,14 +2,14 @@
 class Metr < Formula
   desc "metr gets system metrics."
   homepage "https://github.com/k1LoW/metr"
-  version "0.1.0"
+  version "0.1.1"
 
   if OS.mac?
-    url "https://github.com/k1LoW/metr/releases/download/v0.1.0/metr_v0.1.0_darwin_amd64.zip"
-    sha256 "6eb01029be7bafe50b1877348bb48aaaf0660499dfeed515a48060690a7ca91a"
+    url "https://github.com/k1LoW/metr/releases/download/v0.1.1/metr_v0.1.1_darwin_amd64.zip"
+    sha256 "e8281b5c9914927cdb4b978a7dac58ce4e8ad77880b0b84cbc9d3a3d109ad080"
   elsif OS.linux?
-    url "https://github.com/k1LoW/metr/releases/download/v0.1.0/metr_v0.1.0_linux_amd64.tar.gz"
-    sha256 "e161ebb83b393d8519671c3dfa2c410819c5aab6f85a526c4c76a314d6e4b7f7"
+    url "https://github.com/k1LoW/metr/releases/download/v0.1.1/metr_v0.1.1_linux_amd64.tar.gz"
+    sha256 "f4a4e636fb616c2fd64228c8e346bc5b4917ce7ca7569c1b4136c6da10dbc299"
   end
 
   def install
