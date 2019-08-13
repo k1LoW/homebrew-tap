d0029ea06d5c5591e3da76fe178788250c6b8c98
k1LoW <k1lowxb@gmail.com>
2019-08-13 12:18:28 +0900

    Add imgcat

---
+++ imgcat.rb
@@ -0,0 +1,12 @@
+class Imgcat < Formula
+  desc "Displays one or more images inline at their full size."
+  homepage "https://www.iterm2.com/documentation-images.html"
+  version "0.0.0"
+
+  url "https://iterm2.com/utilities/imgcat"
+  sha256 "b8fd783db29dbdd8a84beabbc251544dcc26bafad3b20c9ff32db7e8c7cd2869"
+
+  def install
+    bin.install 'imgcat'
+  end
+end
