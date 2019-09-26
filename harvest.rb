9dd84ee7fa9d52f9d9dba8bf2cb673d77bbbd89a
k1LoW <k1lowxb@gmail.com>
2019-09-26 16:40:25 +0900

    Brew formula update for harvest version v0.15.0

--- harvest.rb
+++ harvest.rb
@@ -2,14 +2,14 @@
 class Harvest < Formula
   desc "Portable log aggregation tool for middle-scale system operation/observation."
   homepage "https://github.com/k1LoW/harvest"
-  version "0.14.2"
+  version "0.15.0"
 
   if OS.mac?
-    url "https://github.com/k1LoW/harvest/releases/download/v0.14.2/harvest_v0.14.2_darwin_amd64.zip"
-    sha256 "f78d38fc18111c3f093282cf8377592c7bf28c25d88fedeab662d6254756d62e"
+    url "https://github.com/k1LoW/harvest/releases/download/v0.15.0/harvest_v0.15.0_darwin_amd64.zip"
+    sha256 "265857e9f6d0daaf5beb6b83281d364265e0e01d8b12c0e90572fce4957ad858"
   elsif OS.linux?
-    url "https://github.com/k1LoW/harvest/releases/download/v0.14.2/harvest_v0.14.2_linux_amd64.tar.gz"
-    sha256 "b117eba84b4fe647a151fac61f8e3df6fb62014a2d73146204babebe28b332b1"
+    url "https://github.com/k1LoW/harvest/releases/download/v0.15.0/harvest_v0.15.0_linux_amd64.tar.gz"
+    sha256 "71560d580b6203d1140e3bac4aa727cf301d52a643d24f5ba76d96e5bf6edf43"
   end
 
   def install
