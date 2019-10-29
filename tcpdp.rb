b44c9ffc6e6d513ce3cdbe5d0c646672a15a72ce
k1LoW <k1lowxb@gmail.com>
2019-10-29 16:35:26 +0900

    Fix url

--- tcpdp.rb
+++ tcpdp.rb
@@ -3,7 +3,7 @@ class Tcpdp < Formula
   homepage "https://github.com/k1LoW/tcpdp"
   version "0.21.2"
 
-  url "https://github.com/k1LoW/tcpdp/releases/download/v0.21.2/tcpdp_v0.21.2_darwin_amd64.zip"
+  url "https://github.com/k1LoW/tcpdp/releases/download/v0.21.2/tcpdp_v0.21.2_darwin_amd64.tar.gz"
   sha256 "1f437ee10fc60c580b0dbdb440cb889a5575e92d32262f233aad4432aa269f0d"
 
   def install
