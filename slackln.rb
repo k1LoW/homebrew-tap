6232df2c3861a4bdc9be63f82649d1490b2d507c
k1LoW <k1lowxb@gmail.com>
2020-03-04 13:37:58 +0900

    Brew formula update for slackln version v0.2.3

--- slackln.rb
+++ slackln.rb
@@ -2,16 +2,16 @@
 class Slackln < Formula
   desc "Println(Slack)."
   homepage "https://github.com/k1LoW/slackln"
-  version "0.2.2"
+  version "0.2.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/k1LoW/slackln/releases/download/v0.2.2/slackln_v0.2.2_darwin_amd64.zip"
-    sha256 "8d9ec74a07dc97ea7d6922e97b278e5688af5288c64773e8a018d519ccfb9db1"
+    url "https://github.com/k1LoW/slackln/releases/download/v0.2.3/slackln_v0.2.3_darwin_amd64.zip"
+    sha256 "778bc9c1a56a9c92e435f8e31cc27b2c7c40ba2f2bb4a87b338a71f79ba0866b"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/slackln/releases/download/v0.2.2/slackln_v0.2.2_linux_amd64.tar.gz"
-      sha256 "50ead3dd6958b875a1fb5c7ac17a159f6d51e3359056c32a81c63712a89bc452"
+      url "https://github.com/k1LoW/slackln/releases/download/v0.2.3/slackln_v0.2.3_linux_amd64.tar.gz"
+      sha256 "6255564ee8414533880080cfb5f75ee828aa3f017714bd5a3a88ea06144fec9f"
     end
   end
 
