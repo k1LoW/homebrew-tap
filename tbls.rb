4e25ca82618b319ecd01bdb9f7948427b3076f9c
k1LoW <k1lowxb@gmail.com>
2020-05-07 17:29:33 +0900

    Brew formula update for tbls version v1.37.4

--- tbls.rb
+++ tbls.rb
@@ -2,16 +2,16 @@
 class Tbls < Formula
   desc "tbls is a CI-Friendly tool for document a database, written in Go."
   homepage "https://github.com/k1LoW/tbls"
-  version "1.37.3"
+  version "1.37.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/k1LoW/tbls/releases/download/v1.37.3/tbls_v1.37.3_darwin_amd64.zip"
-    sha256 "2136d7e645662255b362ac8777cf898bd98379c990d7865a3e21e112e51cd1e4"
+    url "https://github.com/k1LoW/tbls/releases/download/v1.37.4/tbls_v1.37.4_darwin_amd64.zip"
+    sha256 "89ea39f922285f88455a915f610d000435d8f507ebd404a22b55ad4a72058707"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/tbls/releases/download/v1.37.3/tbls_v1.37.3_linux_amd64.tar.gz"
-      sha256 "e5ec759ccd371dcc3f7057c81f0393d934040e3e65274258abc02c5048e2dc00"
+      url "https://github.com/k1LoW/tbls/releases/download/v1.37.4/tbls_v1.37.4_linux_amd64.tar.gz"
+      sha256 "c3a200b7e42ee5e83c4bdbceee7910ea82797674780336447e345e7f0f0752e3"
     end
   end
 
