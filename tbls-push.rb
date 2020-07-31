dd3fee5048f71c9df1285845f6ee1e942dddfd26
k1LoW <k1lowxb@gmail.com>
2020-07-31 14:24:03 +0900

    Brew formula update for tbls-push version v0.2.0

--- tbls-push.rb
+++ tbls-push.rb
@@ -2,16 +2,16 @@
 class TblsPush < Formula
   desc "tbls-push is an external subcommand of tbls for pushing schema data (schema.json) to target GitHub repository."
   homepage "https://github.com/k1LoW/tbls-push"
-  version "0.1.0"
+  version "0.2.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/k1LoW/tbls-push/releases/download/v0.1.0/tbls-push_v0.1.0_darwin_amd64.zip"
-    sha256 "ec637094e008d52b818732c42bd4ee822cb6adeefe8ef53091399dc927a7c4c2"
+    url "https://github.com/k1LoW/tbls-push/releases/download/v0.2.0/tbls-push_v0.2.0_darwin_amd64.zip"
+    sha256 "ab915fe6e31a12cb85bb02f1bf1e3df2585edfda62c7027034d8c2c5978ebcdf"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/tbls-push/releases/download/v0.1.0/tbls-push_v0.1.0_linux_amd64.tar.gz"
-      sha256 "9cd50f6ceb5e45c64c3c9d9181c184bc610dc5d75b6c9b769b1ca4c60c0bb566"
+      url "https://github.com/k1LoW/tbls-push/releases/download/v0.2.0/tbls-push_v0.2.0_linux_amd64.tar.gz"
+      sha256 "2c67b03b2351d28735c3aaf6c8938d6ee7f42e45d896f1278d0d8ef1e55b4845"
     end
   end
   
