274961151ebb6a27c1ed177d1b6a92015d2aac06
k1LoW <k1lowxb@gmail.com>
2021-01-12 06:31:59 +0900

    Brew formula update for ndiag version v0.9.0

--- ndiag.rb
+++ ndiag.rb
@@ -2,16 +2,16 @@
 class Ndiag < Formula
   desc "ndiag is a high-level architecture diagramming/documentation tool."
   homepage "https://github.com/k1LoW/ndiag"
-  version "0.8.1"
+  version "0.9.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/k1LoW/ndiag/releases/download/v0.8.1/ndiag_v0.8.1_darwin_amd64.zip"
-    sha256 "f190108238c9566c00c611fd9261a2a726b00f2097eff1dc9295fc78f13ca750"
+    url "https://github.com/k1LoW/ndiag/releases/download/v0.9.0/ndiag_v0.9.0_darwin_amd64.zip"
+    sha256 "98a52b8dfc69bb894171d9de3a22cb409596438e861498e3938e9d45a73f6312"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/ndiag/releases/download/v0.8.1/ndiag_v0.8.1_linux_amd64.tar.gz"
-      sha256 "8c294167a92ce1f829dd70cefbe2356db255d82fae4b85af8b53b711b6340a45"
+      url "https://github.com/k1LoW/ndiag/releases/download/v0.9.0/ndiag_v0.9.0_linux_amd64.tar.gz"
+      sha256 "48561cbe680236345b49b5b72186745b12a50c6518e385fae6c7430a9f2de2b8"
     end
   end
 
