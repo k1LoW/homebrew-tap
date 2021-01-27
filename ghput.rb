73b51419fbd7b832e55824a090e7bf2723519476
k1LoW <k1lowxb@gmail.com>
2021-01-27 14:32:15 +0900

    Brew formula update for ghput version v0.8.0

--- ghput.rb
+++ ghput.rb
@@ -2,16 +2,16 @@
 class Ghput < Formula
   desc "ghput is a CI-friendly tool that puts * on GitHub."
   homepage "https://github.com/k1LoW/ghput"
-  version "0.7.0"
+  version "0.8.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/k1LoW/ghput/releases/download/v0.7.0/ghput_v0.7.0_darwin_amd64.zip"
-    sha256 "8af9e3794883bd510481a16c0faf21beca1e8c463d23b9f199f011de0a32cc54"
+    url "https://github.com/k1LoW/ghput/releases/download/v0.8.0/ghput_v0.8.0_darwin_amd64.zip"
+    sha256 "c57452ee5614bcc731b2f9b5f494994909dc33ffd890f7ada4388fe64f6ffdeb"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/ghput/releases/download/v0.7.0/ghput_v0.7.0_linux_amd64.tar.gz"
-      sha256 "84334114c88cb6b33f0a844d08740c437bba3014087c3bd5b6e9a5d8c314f8f9"
+      url "https://github.com/k1LoW/ghput/releases/download/v0.8.0/ghput_v0.8.0_linux_amd64.tar.gz"
+      sha256 "828311c1c1590468beda15d432c4cc5422d2f2ea3690b430c86ffeede74f779c"
     end
   end
 
