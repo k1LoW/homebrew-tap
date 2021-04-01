b9813b67539e8aac7d13288d0ee6d0c69216431e
k1LoW <k1lowxb@gmail.com>
2021-04-01 10:35:05 +0900

    Remove bottle section

--- mingw-w64.rb
+++ mingw-w64.rb
@@ -6,12 +6,6 @@ class MingwW64 < Formula
   license "ZPL-2.1"
   revision 2
 
-  bottle do
-    sha256 "092d1d30ae9f2de677a35f14ec2907d285b85f9b4ed465a506f72a970deea715" => :catalina
-    sha256 "cdefb18e91d0102ba193caa2c6994d83c30742fa03e03e12b3fc5864ca6b003c" => :mojave
-    sha256 "2658e687bbfee45cfa9d0d74c9c129f9ffc2ac1de017143d25b4017aa899404f" => :high_sierra
-  end
-
   # Apple's makeinfo is old and has bugs
   depends_on "texinfo" => :build
 
