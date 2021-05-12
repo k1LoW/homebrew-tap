acd45243729337e59dd5d384f0d1831111d02ec0
k1LoW <k1lowxb@gmail.com>
2021-05-12 12:20:52 +0900

    Brew formula update for octocov version v0.6.1

--- octocov.rb
+++ octocov.rb
@@ -5,17 +5,17 @@
 class Octocov < Formula
   desc "octocov is a tool for collecting code coverage and code to test ratio."
   homepage "https://github.com/k1LoW/octocov"
-  version "0.6.0"
+  version "0.6.1"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/k1LoW/octocov/releases/download/v0.6.0/octocov_v0.6.0_darwin_amd64.zip"
-    sha256 "0d2660b29d7bc67793c96cb1004257b56e44fbbe5ec41f022a9956fceb13c571"
+    url "https://github.com/k1LoW/octocov/releases/download/v0.6.1/octocov_v0.6.1_darwin_amd64.zip"
+    sha256 "e8904c7c4fb246332d5da73ab6102665892d69bf6cbed16207ec5d20b18f3cef"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/k1LoW/octocov/releases/download/v0.6.0/octocov_v0.6.0_linux_amd64.tar.gz"
-    sha256 "ff673c19c3405d60687cc4ba8ff5bf6d9fa4dcff6b4eb29fd3a2fe9d1d54f301"
+    url "https://github.com/k1LoW/octocov/releases/download/v0.6.1/octocov_v0.6.1_linux_amd64.tar.gz"
+    sha256 "9bd0dfdd30a3a24099b4caf19a4c00b66ff38e16cc9c962fae6495a73d68bdaf"
   end
 
   def install
