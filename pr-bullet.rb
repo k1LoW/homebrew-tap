d994e8012db5e085e5db6455e259cbc45f9c8667
k1LoW <k1lowxb@gmail.com>
2021-06-08 11:34:05 +0900

    Brew formula update for pr-bullet version v0.4.0

--- pr-bullet.rb
+++ pr-bullet.rb
@@ -5,17 +5,17 @@
 class PrBullet < Formula
   desc "pr-bullet is a tool for copying pull request to multiple repositories."
   homepage "https://github.com/k1LoW/pr-bullet"
-  version "0.3.1"
+  version "0.4.0"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/k1LoW/pr-bullet/releases/download/v0.3.1/pr-bullet_v0.3.1_darwin_amd64.zip"
-    sha256 "c636f6b76e990213642d5274059d1a30789d620ad6b1f27f4f695a25429960bf"
+    url "https://github.com/k1LoW/pr-bullet/releases/download/v0.4.0/pr-bullet_v0.4.0_darwin_amd64.zip"
+    sha256 "dda071fb745f15b01f6c63c6148f6379834551b720f9124129f5351aff131ce4"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/k1LoW/pr-bullet/releases/download/v0.3.1/pr-bullet_v0.3.1_linux_amd64.tar.gz"
-    sha256 "c3ca3aeea124188f953269204cc4e5caf3897f134aa984096deb01a4ee8bdecb"
+    url "https://github.com/k1LoW/pr-bullet/releases/download/v0.4.0/pr-bullet_v0.4.0_linux_amd64.tar.gz"
+    sha256 "1b9e16dc2f492b035b8ea6726d908495a4baea62a34d861804667b532c71b3c0"
   end
 
   def install
