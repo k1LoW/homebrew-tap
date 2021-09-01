553fde763be52727395332ed0c1841b422ff253d
k1LoW <k1lowxb@gmail.com>
2021-09-02 06:40:27 +0900

    Brew formula update for oshka version v0.4.0

--- oshka.rb
+++ oshka.rb
@@ -5,21 +5,21 @@
 class Oshka < Formula
   desc "oshka is a tool for extracting nested CI/CD supply chains and executing commands."
   homepage "https://github.com/k1LoW/oshka"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/oshka/releases/download/v0.3.0/oshka_v0.3.0_darwin_amd64.zip"
-      sha256 "e51dd0800262ebff192c70d3d0327f2943c458e314cc9a8f8e3ddc0528faf246"
+      url "https://github.com/k1LoW/oshka/releases/download/v0.4.0/oshka_v0.4.0_darwin_amd64.zip"
+      sha256 "0cfb6befac5628c13b6d5117d016ab2965e0e9d4454c7c3959098ba5ea871fff"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/oshka/releases/download/v0.3.0/oshka_v0.3.0_linux_amd64.tar.gz"
-      sha256 "8edb240bf7a1757f704b3b96ccd110316eb03f029910738ee19864276c633e48"
+      url "https://github.com/k1LoW/oshka/releases/download/v0.4.0/oshka_v0.4.0_linux_amd64.tar.gz"
+      sha256 "5f0e31264ff5f5323e2f579ff8600b9a00b8e79e856bddb37d743a6ec9607d98"
     end
   end
 
