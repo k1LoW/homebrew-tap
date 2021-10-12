f7209eda11e3038788f6871ab283f7391a4313ce
k1LoW <k1lowxb@gmail.com>
2021-10-12 17:58:18 +0900

    Brew formula update for octocov version v0.21.1

--- octocov.rb
+++ octocov.rb
@@ -5,21 +5,21 @@
 class Octocov < Formula
   desc "octocov is a tool for collecting code metrics."
   homepage "https://github.com/k1LoW/octocov"
-  version "0.21.0"
+  version "0.21.1"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/octocov/releases/download/v0.21.0/octocov_v0.21.0_darwin_amd64.zip"
-      sha256 "f0fc89680737fe63f6951c2472f3caaa7cafdcff21ddd5f08908e5aef8ee92bd"
+      url "https://github.com/k1LoW/octocov/releases/download/v0.21.1/octocov_v0.21.1_darwin_amd64.zip"
+      sha256 "2090c752bb32b23b4fc13f0fd69db241a7f208f8301af984f21dcba2b5b92fa8"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/octocov/releases/download/v0.21.0/octocov_v0.21.0_linux_amd64.tar.gz"
-      sha256 "5c7adeb9e4cbd2c7949f293ee5ad9ded2dee5e296f9292c367d535318ad37969"
+      url "https://github.com/k1LoW/octocov/releases/download/v0.21.1/octocov_v0.21.1_linux_amd64.tar.gz"
+      sha256 "3e33aa94f8da5c0f83ab37ba8c0aa5d4be5842289a064f7366bd4f73a2ce06a4"
     end
   end
 
