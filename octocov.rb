29dd5664a693441bf85de4986c6e41bad74ff0b2
k1LoW <k1lowxb@gmail.com>
2021-10-30 17:52:36 +0900

    Brew formula update for octocov version v0.28.1

--- octocov.rb
+++ octocov.rb
@@ -5,29 +5,36 @@
 class Octocov < Formula
   desc "octocov is a tool for collecting code metrics."
   homepage "https://github.com/k1LoW/octocov"
-  version "0.28.0"
+  version "0.28.1"
   license "MIT"
-  bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/octocov/releases/download/v0.28.0/octocov_v0.28.0_darwin_amd64.zip"
-      sha256 "bda1d8beed4dee777e1ad82bb516805a8063ea603154868aa0086b8c44fc2b4d"
+      url "https://github.com/k1LoW/octocov/releases/download/v0.28.1/octocov_v0.28.1_darwin_amd64.zip"
+      sha256 "0bdf96de62f269865061d66b302f5d107e24f945d33173ff9bbc814c9936d7b4"
+
+      def install
+        bin.install 'octocov'
+        output = Utils.safe_popen_read("#{bin}/octocov", 'completion', 'bash')
+        (bash_completion/'octocov').write output
+        output = Utils.safe_popen_read("#{bin}/octocov", 'completion', 'zsh')
+        (zsh_completion/'_octocov').write output
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/octocov/releases/download/v0.28.0/octocov_v0.28.0_linux_amd64.tar.gz"
-      sha256 "b0355da79952c9e3fa4ff15a35d5fdea4344edb581764e3e418fdb43fc8e8cf7"
-    end
-  end
+      url "https://github.com/k1LoW/octocov/releases/download/v0.28.1/octocov_v0.28.1_linux_amd64.tar.gz"
+      sha256 "3f219dc390c3b080e474c7effb7bdae2dfb49b841ddcd3edb9c0bbb65db84a3b"
 
-  def install
-    bin.install 'octocov'
-    output = Utils.safe_popen_read("#{bin}/octocov", 'completion', 'bash')
-    (bash_completion/'octocov').write output
-    output = Utils.safe_popen_read("#{bin}/octocov", 'completion', 'zsh')
-    (zsh_completion/'_octocov').write output
+      def install
+        bin.install 'octocov'
+        output = Utils.safe_popen_read("#{bin}/octocov", 'completion', 'bash')
+        (bash_completion/'octocov').write output
+        output = Utils.safe_popen_read("#{bin}/octocov", 'completion', 'zsh')
+        (zsh_completion/'_octocov').write output
+      end
+    end
   end
 end
