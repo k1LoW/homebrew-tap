7bfeaa9e28871bea4b8b9b73a66a15243f9abc40
k1LoW <k1lowxb@gmail.com>
2022-01-05 15:35:49 +0900

    Brew formula update for ndiag version v0.14.1

--- ndiag.rb
+++ ndiag.rb
@@ -5,23 +5,36 @@
 class Ndiag < Formula
   desc "ndiag is a high-level architecture diagramming/documentation tool."
   homepage "https://github.com/k1LoW/ndiag"
-  version "0.14.0"
+  version "0.14.1"
   license "MIT"
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/k1LoW/ndiag/releases/download/v0.14.0/ndiag_v0.14.0_darwin_amd64.zip"
-    sha256 "51285deb5bd8a87edd294e2063787769629f2b6d0f681ffc14ae9e9024ecb94b"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/k1LoW/ndiag/releases/download/v0.14.0/ndiag_v0.14.0_linux_amd64.tar.gz"
-    sha256 "0cd281337ba3476f2af265c988e416c08199281300a9d96669e8bc68ba826311"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/k1LoW/ndiag/releases/download/v0.14.1/ndiag_v0.14.1_darwin_amd64.zip"
+      sha256 "99d7ee7da8095fbfa6e8a61a6123334cf893d990b88241120bf18212ea56286c"
+
+      def install
+        system './ndiag', 'completion', 'bash', '--out', 'ndiag.bash'
+        system './ndiag', 'completion', 'zsh', '--out', 'ndiag.zsh'
+        bin.install 'ndiag'
+        bash_completion.install 'ndiag.bash' => 'ndiag'
+        zsh_completion.install 'ndiag.zsh' => '_ndiag'
+      end
+    end
   end
 
-  def install
-    system './ndiag', 'completion', 'bash', '--out', 'ndiag.bash'
-    system './ndiag', 'completion', 'zsh', '--out', 'ndiag.zsh'
-    bin.install 'ndiag'
-    bash_completion.install 'ndiag.bash' => 'ndiag'
-    zsh_completion.install 'ndiag.zsh' => '_ndiag'
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/k1LoW/ndiag/releases/download/v0.14.1/ndiag_v0.14.1_linux_amd64.tar.gz"
+      sha256 "11c8d3baab709dcc2d9fb10189a93fa099936f339b4843318849d8c5428a7ddf"
+
+      def install
+        system './ndiag', 'completion', 'bash', '--out', 'ndiag.bash'
+        system './ndiag', 'completion', 'zsh', '--out', 'ndiag.zsh'
+        bin.install 'ndiag'
+        bash_completion.install 'ndiag.bash' => 'ndiag'
+        zsh_completion.install 'ndiag.zsh' => '_ndiag'
+      end
+    end
   end
 end
