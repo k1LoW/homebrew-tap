1e4796259a80681a04c14880e5a664186744c086
k1LoW <k1lowxb@gmail.com>
2022-01-19 09:25:37 +0900

    Brew formula update for ndiag version v0.14.2

--- ndiag.rb
+++ ndiag.rb
@@ -5,13 +5,13 @@
 class Ndiag < Formula
   desc "ndiag is a high-level architecture diagramming/documentation tool."
   homepage "https://github.com/k1LoW/ndiag"
-  version "0.14.1"
+  version "0.14.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/ndiag/releases/download/v0.14.1/ndiag_v0.14.1_darwin_amd64.zip"
-      sha256 "99d7ee7da8095fbfa6e8a61a6123334cf893d990b88241120bf18212ea56286c"
+      url "https://github.com/k1LoW/ndiag/releases/download/v0.14.2/ndiag_v0.14.2_darwin_amd64.zip"
+      sha256 "71ff2ebfe91b06fb99d51a1a312bcc69b5cd77e7aed8adccdfabcdfbb4a1e990"
 
       def install
         system './ndiag', 'completion', 'bash', '--out', 'ndiag.bash'
@@ -25,8 +25,8 @@ class Ndiag < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/ndiag/releases/download/v0.14.1/ndiag_v0.14.1_linux_amd64.tar.gz"
-      sha256 "11c8d3baab709dcc2d9fb10189a93fa099936f339b4843318849d8c5428a7ddf"
+      url "https://github.com/k1LoW/ndiag/releases/download/v0.14.2/ndiag_v0.14.2_linux_amd64.tar.gz"
+      sha256 "72bfe741078607a15f028e1061e45a4a03cfb6cab3e170491469669a262a60b9"
 
       def install
         system './ndiag', 'completion', 'bash', '--out', 'ndiag.bash'
