dbd6b9c26fe17eeb737dfe3504c906c4520c9861
k1LoW <k1lowxb@gmail.com>
2022-05-30 15:03:35 +0900

    Brew formula update for runn version v0.18.1

--- runn.rb
+++ runn.rb
@@ -5,13 +5,13 @@
 class Runn < Formula
   desc "runn is a tool for running operations following a scenario."
   homepage "https://github.com/k1LoW/runn"
-  version "0.18.0"
+  version "0.18.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/k1LoW/runn/releases/download/v0.18.0/runn_v0.18.0_darwin_arm64.zip"
-      sha256 "cf2c2e4f3a0dbd30974a693be47a551a79649301dc2a5bd7e075f1cc22dab189"
+      url "https://github.com/k1LoW/runn/releases/download/v0.18.1/runn_v0.18.1_darwin_arm64.zip"
+      sha256 "03a9c923f8a4e59ca73e9cbe20b1e1b2e8acda5ed7f1a41f8cd19d817854f0c4"
 
       def install
         bin.install 'runn'
@@ -22,8 +22,8 @@ class Runn < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/runn/releases/download/v0.18.0/runn_v0.18.0_darwin_amd64.zip"
-      sha256 "b2893e060a8bd9f2084f5aabeef9bfff93dd91af0a721162278076116097f634"
+      url "https://github.com/k1LoW/runn/releases/download/v0.18.1/runn_v0.18.1_darwin_amd64.zip"
+      sha256 "0a839c0e5fc13e43cd89bd1a49dc332d6965073cfdfbb2c5092475d05fa9ad4c"
 
       def install
         bin.install 'runn'
@@ -37,8 +37,8 @@ class Runn < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/runn/releases/download/v0.18.0/runn_v0.18.0_linux_amd64.tar.gz"
-      sha256 "ffa3b0f55785b9f23c65d43d0157bf40f97fdf268cf16380e33511083fd4d002"
+      url "https://github.com/k1LoW/runn/releases/download/v0.18.1/runn_v0.18.1_linux_amd64.tar.gz"
+      sha256 "2bf8f0ddb6714969255ab60cb4d299f2e6375fe7a6b7f7146ad7a0c5211412f5"
 
       def install
         bin.install 'runn'
