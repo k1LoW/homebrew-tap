e4c05dfd5aca7ccde42e9cfc0e763294fa711fcd
k1LoW <k1lowxb@gmail.com>
2022-09-01 17:00:41 +0900

    Brew formula update for octocov version v0.41.1

--- octocov.rb
+++ octocov.rb
@@ -5,14 +5,14 @@
 class Octocov < Formula
   desc "octocov is a toolkit for collecting code metrics."
   homepage "https://github.com/k1LoW/octocov"
-  version "0.41.0"
+  version "0.41.1"
   license "MIT"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/k1LoW/octocov/releases/download/v0.41.0/octocov_v0.41.0_darwin_arm64.zip"
-      sha256 "900d4ffd6760b2860c9807e7fd9ee76302ed4149d07f65c811fbf06a0aaf1f82"
+      url "https://github.com/k1LoW/octocov/releases/download/v0.41.1/octocov_v0.41.1_darwin_arm64.zip"
+      sha256 "2c0af8a439716b51ebedec0e7c4cb7106d97ddc1bb58b2bca2dfd9e0fbd312f1"
 
       def install
         bin.install 'octocov'
@@ -23,8 +23,8 @@ class Octocov < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/octocov/releases/download/v0.41.0/octocov_v0.41.0_darwin_amd64.zip"
-      sha256 "083c579113e3e98ed61ca89951fdf219d7f8537d1ff9b5cb77dae21c5ad8864e"
+      url "https://github.com/k1LoW/octocov/releases/download/v0.41.1/octocov_v0.41.1_darwin_amd64.zip"
+      sha256 "3695fdacfb75f28f8e9735908a42bbe390350d662bb9adbbb1ebf95687c2c642"
 
       def install
         bin.install 'octocov'
