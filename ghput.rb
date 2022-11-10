75f34e31f40cc24988da0724ca8f393d1d781792
k1LoW <k1LoW@users.noreply.github.com>
2022-11-10 18:04:01 +0000

    Commit from GitHub Actions (Update formulae)

--- ghput.rb
+++ ghput.rb
@@ -5,14 +5,14 @@
 class Ghput < Formula
   desc "ghput is a CI-friendly tool that puts * on GitHub."
   homepage "https://github.com/k1LoW/ghput"
-  version "0.12.2"
+  version "0.13.0"
   license "MIT"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/k1LoW/ghput/releases/download/v0.12.2/ghput_v0.12.2_darwin_arm64.zip"
-      sha256 "0e5d24eba56338c5bf4b5e814f43c735ff9d6fa8cf7e35cc048c8617bb9dfcc8"
+      url "https://github.com/k1LoW/ghput/releases/download/v0.13.0/ghput_v0.13.0_darwin_arm64.zip"
+      sha256 "db4016ca8266143b991790216284774f79e48388a95b5ef4c988ccd73acc5af5"
 
       def install
         system './ghput', 'completion', 'bash', '--out', 'ghput.bash'
@@ -23,8 +23,8 @@ class Ghput < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/ghput/releases/download/v0.12.2/ghput_v0.12.2_darwin_amd64.zip"
-      sha256 "f8585504398366d00757c0acb69af42428eba93d9b0a6712537927077f1125ba"
+      url "https://github.com/k1LoW/ghput/releases/download/v0.13.0/ghput_v0.13.0_darwin_amd64.zip"
+      sha256 "d2e90e4d15e33d4e10c8c7da3d03e2bb8ac85be23eeb81c6c7059f4156f755b9"
 
       def install
         system './ghput', 'completion', 'bash', '--out', 'ghput.bash'
--- runn.rb
+++ runn.rb
@@ -1,24 +1,24 @@
 class Runn < Formula
   desc 'runn is a tool for running operations following a scenario.'
   homepage 'https://github.com/k1LoW/runn'
-  version '0.47.2'
+  version '0.47.3'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/runn/releases/download/v0.47.2/runn_v0.47.2_darwin_arm64.zip'
-      sha256 '3282fa9f545dc0508d1a8d60ab36cbd303d724cea50763300d82b252f9cf3c94'
+      url 'https://github.com/k1LoW/runn/releases/download/v0.47.3/runn_v0.47.3_darwin_arm64.zip'
+      sha256 'ae105cc832270feb55cd3036ba4ecf5df418cb94600c0d1f905d652e1c448203'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/runn/releases/download/v0.47.2/runn_v0.47.2_darwin_amd64.zip'
-      sha256 '47f23b33c9731fd015aac381040bf94406aea96a99aedb373be962291195e868'
+      url 'https://github.com/k1LoW/runn/releases/download/v0.47.3/runn_v0.47.3_darwin_amd64.zip'
+      sha256 '20fa1db24a3390b3e35e608485f0fc8f6e480cf5b9e40c0453f9b85154dccbf3'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/runn/releases/download/v0.47.2/runn_v0.47.2_linux_amd64.tar.gz'
-      sha256 'bcc7eee5d3ee07d43ac41b0e30cb26357d7622d59ccf133ba6e3f8e65287bd93'
+      url 'https://github.com/k1LoW/runn/releases/download/v0.47.3/runn_v0.47.3_linux_amd64.tar.gz'
+      sha256 '06688e4b047cb57fa12fa27203555a979f1827a75d5518c738543f5907199260'
     end
   end
 
