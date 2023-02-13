c68426720f47e9f4ca387d70af327e47ead5876a
k1LoW <k1LoW@users.noreply.github.com>
2023-02-13 18:08:16 +0000

    Commit from GitHub Actions (Update formulae)

--- gh-setup.rb
+++ gh-setup.rb
@@ -1,24 +1,24 @@
 class GhSetup < Formula
   desc ':octocat: Setup asset of Github releases.'
-  version '0.6.0'
+  version '0.7.1'
   homepage 'https://github.com/k1LoW/gh-setup'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/gh-setup/releases/download/v0.6.0/gh-setup_v0.6.0_darwin_arm64.zip'
-      sha256 '4add3bec65e49391bc2a1f030b3f9ae3041113ea0d77903a649cd16227be8359'
+      url 'https://github.com/k1LoW/gh-setup/releases/download/v0.7.1/gh-setup_v0.7.1_darwin_arm64.zip'
+      sha256 '0d2fbe37fe470877cfe8a9c7e6cb34fe50739d1b887053bb8a6ccf5c09d194f7'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gh-setup/releases/download/v0.6.0/gh-setup_v0.6.0_darwin_amd64.zip'
-      sha256 'ba242bd7c23c90f214f276abddb580e8c45c04c594576a1b3d9f3729d409e6f1'
+      url 'https://github.com/k1LoW/gh-setup/releases/download/v0.7.1/gh-setup_v0.7.1_darwin_amd64.zip'
+      sha256 '54d310fcaebbe0dabf8f0f0a3882c2d00cbe1404854a8c234925d82546f833bd'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gh-setup/releases/download/v0.6.0/gh-setup_v0.6.0_linux_amd64.tar.gz'
-      sha256 '0e392fed26d011004cf06aa8973543f7f45ecfd87585b3698514b2c067bc6252'
+      url 'https://github.com/k1LoW/gh-setup/releases/download/v0.7.1/gh-setup_v0.7.1_linux_amd64.tar.gz'
+      sha256 '24f5134e08a5a3253d98797e0d70b5a3ee432e6d0fd470cc9dca477a507dcb14'
     end
   end
 
--- runn.rb
+++ runn.rb
@@ -1,24 +1,24 @@
 class Runn < Formula
   desc 'runn is a tool for running operations following a scenario.'
   homepage 'https://github.com/k1LoW/runn'
-  version '0.59.2'
+  version '0.59.3'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/runn/releases/download/v0.59.2/runn_v0.59.2_darwin_arm64.zip'
-      sha256 '7afc87be383e3166f0fb8ecd1ec4c408b4cb7c8f179e7071f966c44b92085c23'
+      url 'https://github.com/k1LoW/runn/releases/download/v0.59.3/runn_v0.59.3_darwin_arm64.zip'
+      sha256 'ab57c998e1b66e5786d9d5c9eb5ab71e548a79bd70e886f84d84c664245ef3e9'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/runn/releases/download/v0.59.2/runn_v0.59.2_darwin_amd64.zip'
-      sha256 'e7c6334f68a947fe3405048ea451016895ae2f885b9c2127d23aba8c18b9966a'
+      url 'https://github.com/k1LoW/runn/releases/download/v0.59.3/runn_v0.59.3_darwin_amd64.zip'
+      sha256 '7c97492330f689296c81472d2415e2a9b0d64436ff3bf8b88bfe5f918ce97c66'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/runn/releases/download/v0.59.2/runn_v0.59.2_linux_amd64.tar.gz'
-      sha256 '6cd69d0b8c93ab9e03cfa04e833dbf9d84afbb7bf9a06d6262aa9c37c1fa7adf'
+      url 'https://github.com/k1LoW/runn/releases/download/v0.59.3/runn_v0.59.3_linux_amd64.tar.gz'
+      sha256 '1f92545e4bc70ebd9ae8e14af7ccd54488ddfe8476785714deeddbf9ba606406'
     end
   end
 
