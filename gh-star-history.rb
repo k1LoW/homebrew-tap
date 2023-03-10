5b016ee429525ad783aec0f0ec8b970172143249
k1LoW <k1lowxb@gmail.com>
2023-03-10 20:18:49 +0900

    Brew formula update for gh-star-history version v0.2.0

--- gh-star-history.rb
+++ gh-star-history.rb
@@ -5,21 +5,21 @@
 class GhStarHistory < Formula
   desc "Show star history of repositories."
   homepage "https://github.com/k1LoW/gh-star-history"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/k1LoW/gh-star-history/releases/download/v0.1.0/gh-star-history_v0.1.0_darwin_arm64.zip"
-      sha256 "8c2f293551bdf00ea0a44b708286be6def1303cab57712b32131ed9af25ca822"
+      url "https://github.com/k1LoW/gh-star-history/releases/download/v0.2.0/gh-star-history_v0.2.0_darwin_arm64.zip"
+      sha256 "17a6c05d672cd9b5ab0c667c8b733d673e01f7533420f3d38d58e78dc7ff03d5"
 
       def install
         bin.install "gh-star-history"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/gh-star-history/releases/download/v0.1.0/gh-star-history_v0.1.0_darwin_amd64.zip"
-      sha256 "f1c5e861fed9daa15871ad2c62ce1d6741f69991a5012d6ae600c5aa64730c1a"
+      url "https://github.com/k1LoW/gh-star-history/releases/download/v0.2.0/gh-star-history_v0.2.0_darwin_amd64.zip"
+      sha256 "04e6bee5a4a3fa9d80a04fc16a61d87a7db58a869309c17e4cfda1a4d0bd65e7"
 
       def install
         bin.install "gh-star-history"
@@ -29,8 +29,8 @@ class GhStarHistory < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/gh-star-history/releases/download/v0.1.0/gh-star-history_v0.1.0_linux_amd64.tar.gz"
-      sha256 "ae2b1ad1d129cc1c00a7bbb47c1c9cbd9fdb833a3145c15bd5496f998e621ab9"
+      url "https://github.com/k1LoW/gh-star-history/releases/download/v0.2.0/gh-star-history_v0.2.0_linux_amd64.tar.gz"
+      sha256 "707e00648989db00390767bd47f5b667860a2366d5192fdf089dd137a8c29e4f"
 
       def install
         bin.install "gh-star-history"
