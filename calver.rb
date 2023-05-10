b131278e951a10dc47ffd64686215d139972a535
k1LoW <k1LoW@users.noreply.github.com>
2023-05-10 06:03:36 +0000

    Commit from GitHub Actions (Update formulae)

--- calver.rb
+++ calver.rb
@@ -1,23 +1,23 @@
 class Calver < Formula
   desc 'calver package provides the ability to work with Calendar Versioning in Go.'
-  version '0.3.0'
+  version '0.4.0'
   homepage 'https://github.com/k1LoW/calver'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/calver/releases/download/v0.3.0/calver_v0.3.0_darwin_arm64.zip'
-      sha256 'f41dd7d15aa4c4d6838c65b4b1894dfcdae4eac3f00d86a4b35dd255bc0134f3'
+      url 'https://github.com/k1LoW/calver/releases/download/v0.4.0/calver_v0.4.0_darwin_arm64.zip'
+      sha256 '27727f2e4b41d1c70b7d574de370a36da8b1dfd92b2019e8b450a520ce909d51'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/calver/releases/download/v0.3.0/calver_v0.3.0_darwin_amd64.zip'
-      sha256 'da97ade038f669757145cd46b46f969d2b81119771000cfe521bc284676d735f'
+      url 'https://github.com/k1LoW/calver/releases/download/v0.4.0/calver_v0.4.0_darwin_amd64.zip'
+      sha256 '63fc08647d401977fc067bee63f9c278bb70eb1bfc1e02132750b85b7a53e71b'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/calver/releases/download/v0.3.0/calver_v0.3.0_linux_amd64.tar.gz'
-      sha256 'ee817628fc90145943678434a7d8848ea95c293b28408bc69bf7584d36746987'
+      url 'https://github.com/k1LoW/calver/releases/download/v0.4.0/calver_v0.4.0_linux_amd64.tar.gz'
+      sha256 'ae3fd771495a2a5ab7e8fb7a9b170d619c7d59d88c72eb43062f589346c8456a'
     end
   end
 
--- gh-setup.rb
+++ gh-setup.rb
@@ -1,24 +1,24 @@
 class GhSetup < Formula
   desc ':octocat: Setup asset of Github releases.'
-  version '1.4.0'
+  version '1.4.2'
   homepage 'https://github.com/k1LoW/gh-setup'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.4.0/gh-setup_v1.4.0_darwin_arm64.zip'
-      sha256 'd4d821c02a8734f5d6d14fb07bb9d7fd382dbce987f2662ef356cf791a7692f2'
+      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.4.2/gh-setup_v1.4.2_darwin_arm64.zip'
+      sha256 '257f631346ac6f8f060f15a71274551fdcce21e0921c07a7c81db69f98eef36b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.4.0/gh-setup_v1.4.0_darwin_amd64.zip'
-      sha256 'c96dc27c8317e0bf568954c51546e7203ca20b3695915b20b35408fb123bae42'
+      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.4.2/gh-setup_v1.4.2_darwin_amd64.zip'
+      sha256 '6cf8e25587a6c048d5c421b2adaf43ee3333d9d1f752322d668f40e8b209d1d9'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.4.0/gh-setup_v1.4.0_linux_amd64.tar.gz'
-      sha256 '597e4b94d7e77fe0b72f11aa4d4f70750c327ff2b62af624e7c02a1c4539b3d2'
+      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.4.2/gh-setup_v1.4.2_linux_amd64.tar.gz'
+      sha256 '89957aebd0267f16dedd6a01626566f0e896b8310fa46e3d3e9b7b6172bc3edb'
     end
   end
 
