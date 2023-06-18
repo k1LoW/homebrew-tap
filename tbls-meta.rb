9ec2b5b5a934f17098896369ba5a9a3341e3d4ac
k1LoW <k1LoW@users.noreply.github.com>
2023-06-18 04:26:47 +0000

    Commit from GitHub Actions (Update formulae)

--- tbls-meta.rb
+++ tbls-meta.rb
@@ -1,27 +1,27 @@
 class TblsMeta < Formula
   desc 'tbls-meta is an external subcommand of tbls for applying metadata managed by tbls to the datasource.'
-  version '0.3.0'
+  version '0.3.1'
   homepage 'https://github.com/k1LoW/tbls-meta'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tbls-meta/releases/download/v0.3.0/tbls-meta_v0.3.0_darwin_arm64.zip'
-      sha256 '57a621ee685b2364b58c91e1f4106c0ff40f45b95dedcd4fd2f835fd30d84853'
+      url 'https://github.com/k1LoW/tbls-meta/releases/download/v0.3.1/tbls-meta_v0.3.1_darwin_arm64.zip'
+      sha256 '3306d2092b1ee48f80da705f8679116dc55f52945c7e9080a8d7dbc4f8b019a9'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls-meta/releases/download/v0.3.0/tbls-meta_v0.3.0_darwin_amd64.zip'
-      sha256 '384bace788df4fef84656ca1225ef954a8f6bbf633396ff7d56aff2f277b3f51'
+      url 'https://github.com/k1LoW/tbls-meta/releases/download/v0.3.1/tbls-meta_v0.3.1_darwin_amd64.zip'
+      sha256 '4ff782bff8bbdcca0b45286cf4d694ebbfe2d26ead47e32dddeda3d61838e702'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tbls-meta/releases/download/v0.3.0/tbls-meta_v0.3.0_linux_arm64.tar.gz'
-      sha256 '34189092a311672478693ba2753d127d579fde4fc59fdd881bf2b85965ea4fd4'
+      url 'https://github.com/k1LoW/tbls-meta/releases/download/v0.3.1/tbls-meta_v0.3.1_linux_arm64.tar.gz'
+      sha256 '486249ba6d838376d81435835f1fc5c403457281174722f637bc4da3e5e29b6a'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls-meta/releases/download/v0.3.0/tbls-meta_v0.3.0_linux_amd64.tar.gz'
-      sha256 'b8133892dacbc1527476d4c1a90cbebf220b5b1b73328177c347a3c25c7fc3a2'
+      url 'https://github.com/k1LoW/tbls-meta/releases/download/v0.3.1/tbls-meta_v0.3.1_linux_amd64.tar.gz'
+      sha256 '5c38d73970e292795e30303520d0d59a9b8a137f233db908f66dd7bec61fe4d2'
     end
   end
 
