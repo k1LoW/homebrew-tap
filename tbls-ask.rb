0ebbcbd661d0cef5b7124c8b8aa133c39635f4c3
k1LoW <k1LoW@users.noreply.github.com>
2023-06-29 06:04:25 +0000

    Commit from GitHub Actions (Update formulae)

--- tbls-ask.rb
+++ tbls-ask.rb
@@ -1,27 +1,27 @@
 class TblsAsk < Formula
   desc 'tbls-ask is an external subcommand of tbls for asking OpenAI using the datasource.'
-  version '0.0.2'
+  version '0.0.3'
   homepage 'https://github.com/k1LoW/tbls-ask'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.0.2/tbls-ask_v0.0.2_darwin_arm64.zip'
-      sha256 'a0449c90a581ddcef83348f7242492c2e268ec1160b424e1ad597873397a889e'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.0.3/tbls-ask_v0.0.3_darwin_arm64.zip'
+      sha256 '43c5d76facba0df680be9531edd15b8386bedfb6e3c516885e8e02663012e742'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.0.2/tbls-ask_v0.0.2_darwin_amd64.zip'
-      sha256 '71113fbae39170208f56807d71724fb0390abe53beae5806d510ba7803558cf2'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.0.3/tbls-ask_v0.0.3_darwin_amd64.zip'
+      sha256 '5154d94e800ee2d686111b7912d84d85da27beb12a86a535d82a8c1dcaa70611'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.0.2/tbls-ask_v0.0.2_linux_arm64.tar.gz'
-      sha256 '50d8369e7e7f8e3f8851402f37412bd17a602a0cfd51f8e2801518c8fcdfd100'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.0.3/tbls-ask_v0.0.3_linux_arm64.tar.gz'
+      sha256 'cda08582634be1616f8268c078a9ae42e96a06f285bfff9f45a88db86affce75'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.0.2/tbls-ask_v0.0.2_linux_amd64.tar.gz'
-      sha256 'e2e70f0455b1302ebfa4d099d7b8f339f575d1292cef9c5118a8680615289e3f'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.0.3/tbls-ask_v0.0.3_linux_amd64.tar.gz'
+      sha256 '322878cde53acf72bef2d3bd66a2ecd6acd41975a19a234b52a9b3884cd670ed'
     end
   end
 
