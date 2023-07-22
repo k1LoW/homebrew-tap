c1385ae87af4497bd9efe2d52d09a9ee68ff6db3
k1LoW <k1LoW@users.noreply.github.com>
2023-07-22 18:03:48 +0000

    Commit from GitHub Actions (Update formulae)

--- alp.rb
+++ alp.rb
@@ -1,14 +1,14 @@
 class Alp < Formula
   desc 'Access Log Profiler'
   homepage 'https://github.com/tkuchiki/alp'
-  version '1.0.12'
+  version '1.0.13'
 
   if OS.mac?
-    url 'https://github.com/tkuchiki/alp/releases/download/v1.0.12/alp_darwin_amd64.tar.gz'
-    sha256 '0a5bae8a5766ab4bd2cc4bca6c1ac4ca78104dada9b9a936d050f6ccdb3a92a6'
+    url 'https://github.com/tkuchiki/alp/releases/download/v1.0.13/alp_darwin_amd64.tar.gz'
+    sha256 '845ee66a8431d3895182167aa9576082646ae6e8e100225b63c496c82c86d0e1'
   elsif OS.linux?
-    url 'https://github.com/tkuchiki/alp/releases/download/v1.0.12/alp_linux_amd64.tar.gz'
-    sha256 'c47ab3a4bf556babe728bc17da02d9aabb2087ea26e8adf5d10da8b3d5466018'
+    url 'https://github.com/tkuchiki/alp/releases/download/v1.0.13/alp_linux_amd64.tar.gz'
+    sha256 '5d63c6712522f901c379bfbcbf1562a70c82410409434516b42adf3478143a44'
   end
 
   def install
