1052e61df7349f6d7733c8cced139799f9625d1e
k1LoW <k1LoW@users.noreply.github.com>
2023-09-06 18:03:39 +0000

    Commit from GitHub Actions (Update formulae)

--- octocov.rb
+++ octocov.rb
@@ -1,24 +1,24 @@
 class Octocov < Formula
   desc 'octocov is a toolkit for collecting code metrics (code coverage, code to test ratio and test execution time).'
-  version '0.51.4'
+  version '0.51.6'
   license "MIT"
   homepage 'https://github.com/k1LoW/octocov'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.51.4/octocov_v0.51.4_darwin_arm64.zip'
-      sha256 '599bfa61ecdb22bef5da5558c2cebb40954e7e5d9575f0502b840d0d221c4bea'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.51.6/octocov_v0.51.6_darwin_arm64.zip'
+      sha256 '387cf17dbbd10554509bf72df0ddd15ae15d09f18b58f5ddb42f3e22ad24b8cc'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.51.4/octocov_v0.51.4_darwin_amd64.zip'
-      sha256 '9190c29eae886ea0ea3617012aceb83632048d6974180080f706aec2e0435c12'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.51.6/octocov_v0.51.6_darwin_amd64.zip'
+      sha256 '6c2ef5f988eab20ff20a7c0be3febbd5e29f327abb469ff60a4f678f3c0cdfe8'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.51.4/octocov_v0.51.4_linux_amd64.tar.gz'
-      sha256 'cc17f80a598930b43ea9cc00d8c2103f7ef8ed72a6b165ee60efd5c2f9d3dd66'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.51.6/octocov_v0.51.6_linux_amd64.tar.gz'
+      sha256 '94b550f4bbb9205af643caeabefc435bbece25e95100f268e7d1b4a3d1b97bd8'
     end
   end
 
