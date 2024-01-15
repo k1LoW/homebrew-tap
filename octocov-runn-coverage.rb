5f687b41644620a4f2e883bb5f6ccee0463cc959
k1LoW <k1LoW@users.noreply.github.com>
2024-01-15 06:04:04 +0000

    Commit from GitHub Actions (Update formulae)

--- octocov-runn-coverage.rb
+++ octocov-runn-coverage.rb
@@ -1,27 +1,27 @@
 class OctocovRunnCoverage < Formula
   desc 'Generate octocov custom metrics JSON from the output of 'runn coverage'.'
-  version '0.0.1'
+  version '0.0.2'
   homepage 'https://github.com/k1LoW/octocov-runn-coverage'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.0.1/octocov-runn-coverage_v0.0.1_darwin_arm64.zip'
-      sha256 '03f1f45a31290dede39a479e03b29025be9fa5c17950b19c560b31549a0c076a'
+      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.0.2/octocov-runn-coverage_v0.0.2_darwin_arm64.zip'
+      sha256 'fa12b411cfd4a7cf8dc4392d4947e1735d120e7c5c3e4dfb3b2d952589a4b9fb'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.0.1/octocov-runn-coverage_v0.0.1_darwin_amd64.zip'
-      sha256 'fb0735ce7e7f3681775d0b564e43110406d777109695d61edb5c58040984d9e8'
+      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.0.2/octocov-runn-coverage_v0.0.2_darwin_amd64.zip'
+      sha256 '4624e441e6347004e08e609ea608c197e3d6b058ec977d41d0db6e7176f6f62d'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.0.1/octocov-runn-coverage_v0.0.1_linux_arm64.tar.gz'
-      sha256 '062bc8390e801bbe149a37eb4bd1ce4b11f4aa2dc6ed3dae8b2ba0aa7d5e8445'
+      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.0.2/octocov-runn-coverage_v0.0.2_linux_arm64.tar.gz'
+      sha256 '972172dbb42732fb6e4d71b8e4f0f943307d8a9caaf138770daacf4724cecf3c'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.0.1/octocov-runn-coverage_v0.0.1_linux_amd64.tar.gz'
-      sha256 'ccc2cfedc9c7d73066d83e72f53ed0961e1c97c289d59d989cc28d84fbb1992f'
+      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.0.2/octocov-runn-coverage_v0.0.2_linux_amd64.tar.gz'
+      sha256 'acb8031a56513c5a87541c6d666dbf3deccc798168f9f20b56109dfa1e0e2a76'
     end
   end
 
