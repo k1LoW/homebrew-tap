5440c77d98242c1bc59c68befe1566ff719e11b9
k1LoW <k1LoW@users.noreply.github.com>
2024-02-26 18:03:39 +0000

    Commit from GitHub Actions (Update formulae)

--- gh-setup.rb
+++ gh-setup.rb
@@ -1,24 +1,24 @@
 class GhSetup < Formula
   desc ':octocat: Setup asset of Github releases.'
-  version '1.8.2'
+  version '1.8.3'
   homepage 'https://github.com/k1LoW/gh-setup'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.8.2/gh-setup_v1.8.2_darwin_arm64.zip'
-      sha256 '3ca3d781c3e0b30e3ada56cca92346aa93257c1605a774a4318a6b6a6b18dba7'
+      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.8.3/gh-setup_v1.8.3_darwin_arm64.zip'
+      sha256 'c847d7d2ee7634939e1d6e666813ff390573ea813e50758088ecf11155c3625b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.8.2/gh-setup_v1.8.2_darwin_amd64.zip'
-      sha256 '97985407e3aaf3776e477e73db471c83173456cda19e389685a98a69e6b2efcc'
+      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.8.3/gh-setup_v1.8.3_darwin_amd64.zip'
+      sha256 '9dfc1eb0540bc4da7fcc97e54cbe4ee41fff89c37d127e92208203d886e94c93'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.8.2/gh-setup_v1.8.2_linux_amd64.tar.gz'
-      sha256 '586a21de5096827679300afc25983019bc26acd1f0fe9bf88f43da87f5bba6ef'
+      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.8.3/gh-setup_v1.8.3_linux_amd64.tar.gz'
+      sha256 '83033ee211ac5b926efb75d51b5fb0d82411164510685efca4c97390432eac7e'
     end
   end
 
--- octocov.rb
+++ octocov.rb
@@ -1,24 +1,24 @@
 class Octocov < Formula
   desc 'octocov is a toolkit for collecting code metrics (code coverage, code to test ratio and test execution time).'
-  version '0.58.0'
+  version '0.58.1'
   license "MIT"
   homepage 'https://github.com/k1LoW/octocov'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.58.0/octocov_v0.58.0_darwin_arm64.zip'
-      sha256 '924828a35fd5d642b322720c832ac1772b9e2acdad129cce4363915bba97fa7b'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.58.1/octocov_v0.58.1_darwin_arm64.zip'
+      sha256 '0006a14d16cb10d2a3ddf99a2864b37159fe9057a604f7c9cf1303354c6241ad'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.58.0/octocov_v0.58.0_darwin_amd64.zip'
-      sha256 'edda3f28547c833b0bc4519fbbf8d983059ea5bd05cd29f43f93fa93a25a0008'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.58.1/octocov_v0.58.1_darwin_amd64.zip'
+      sha256 '91e2b0dd775c6bff3d1d15e73945e8f5f4ecc4310ac3dd24a4687911d312a84d'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.58.0/octocov_v0.58.0_linux_amd64.tar.gz'
-      sha256 '9d47a1fd054b339ca38808e852771f534675f2aef109f789ba7708a216698dd4'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.58.1/octocov_v0.58.1_linux_amd64.tar.gz'
+      sha256 'ea77ae4248951309b11943e6fc68165f5d0beab5fa89878e66ac3cdacd6f367e'
     end
   end
 
