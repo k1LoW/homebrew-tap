3e18dc7cd290e57ec6376cc5beb7838cc84a597d
k1LoW <k1LoW@users.noreply.github.com>
2024-03-28 18:03:47 +0000

    Commit from GitHub Actions (Update formulae)

--- tcpdp.rb
+++ tcpdp.rb
@@ -1,7 +1,7 @@
 class Tcpdp < Formula
   desc "tcpdp is TCP dump tool with custom dumper and structured logger written in Go."
   homepage "https://github.com/k1LoW/tcpdp"
-  version "0.22.2"
+  version "0.23.9"
 
   url "https://github.com/k1LoW/tcpdp/releases/download/v0.22.2/tcpdp_v0.22.2_darwin_amd64.tar.gz"
   sha256 "b60ba68b5d0144fe95152b933d9eda1c336d715dde1ccb54209c528982e708d1"
