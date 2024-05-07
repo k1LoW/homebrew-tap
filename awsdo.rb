8245946409039924cc2ac8f47a7c045e33db7c59
k1LoW <k1LoW@users.noreply.github.com>
2024-05-07 13:19:38 +0000

    Commit from GitHub Actions (Update formulae)

--- awsdo.rb
+++ awsdo.rb
@@ -5,21 +5,21 @@
 class Awsdo < Formula
   desc "awsdo is a tool to do anything using AWS temporary credentials."
   homepage "https://github.com/k1LoW/awsdo"
-  version "0.11.1"
+  version "0.12.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/k1LoW/awsdo/releases/download/v0.11.1/awsdo_v0.11.1_darwin_arm64.zip"
-      sha256 "1824c714a954b449c6950dffe8b438f3aec4c27fe0f9a50023d9306ac096d0d0"
+      url "https://github.com/k1LoW/awsdo/releases/download/v0.12.0/awsdo_v0.12.0_darwin_arm64.zip"
+      sha256 "a0c8751d93f0c2713afc1af88a8f869d70dd2116ba904e5d81c04f4ec728d8ef"
 
       def install
         bin.install "awsdo"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/awsdo/releases/download/v0.11.1/awsdo_v0.11.1_darwin_amd64.zip"
-      sha256 "961c941c0cfbfe7e65298c7fddb7f916202c54d0103a7e1e770ffae7127e4e43"
+      url "https://github.com/k1LoW/awsdo/releases/download/v0.12.0/awsdo_v0.12.0_darwin_amd64.zip"
+      sha256 "a8a4a1731e50695f9ccfbb650b1c61463643d47e8f497975e96d26db85178ae6"
 
       def install
         bin.install "awsdo"
@@ -29,16 +29,16 @@ class Awsdo < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/k1LoW/awsdo/releases/download/v0.11.1/awsdo_v0.11.1_linux_arm64.tar.gz"
-      sha256 "6db72aad12647bc2822d28e2605f1ea12552be1182ac9fa09fa3e8d45821cd1e"
+      url "https://github.com/k1LoW/awsdo/releases/download/v0.12.0/awsdo_v0.12.0_linux_arm64.tar.gz"
+      sha256 "e7fe98e59d0a7e634f6568400a13cdf6fd01d2f4ad00b3748ba14484010017b4"
 
       def install
         bin.install "awsdo"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/k1LoW/awsdo/releases/download/v0.11.1/awsdo_v0.11.1_linux_amd64.tar.gz"
-      sha256 "40caa3400d4ffd2879fad5c1d70dce39660a1d9fe85224fd17b926897f9e7cfa"
+      url "https://github.com/k1LoW/awsdo/releases/download/v0.12.0/awsdo_v0.12.0_linux_amd64.tar.gz"
+      sha256 "a27554a88d3528347f48e258dbc9ec311e81e60cce0300d211eb15eedf7c0ba8"
 
       def install
         bin.install "awsdo"
