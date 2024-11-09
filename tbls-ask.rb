1c23f2fe33f3b93e44b5a266e7e54908233d6548
k1LoW <k1LoW@users.noreply.github.com>
2024-11-09 18:03:59 +0000

    Commit from GitHub Actions (Update formulae)

--- tbls-ask.rb
+++ tbls-ask.rb
@@ -1,27 +1,27 @@
 class TblsAsk < Formula
   desc 'tbls-ask is an external subcommand of tbls for asking OpenAI using the datasource.'
-  version '0.2.0'
+  version '0.3.0'
   homepage 'https://github.com/k1LoW/tbls-ask'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.2.0/tbls-ask_v0.2.0_darwin_arm64.zip'
-      sha256 '76fdaa7e07f6308f53a30bc8ac86346fed1c9c6918a42141d1016730c7451dcb'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.3.0/tbls-ask_v0.3.0_darwin_arm64.zip'
+      sha256 '0a09c7cc266f119ed98183efeba912887de35fbf4cdd8f2b68df86e17dbcd7c0'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.2.0/tbls-ask_v0.2.0_darwin_amd64.zip'
-      sha256 'f2a94893b92978320f9965e1ddfbdee88decefe7397b1dbb81e259ffe355db2f'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.3.0/tbls-ask_v0.3.0_darwin_amd64.zip'
+      sha256 '72822afc47173222dc4be40fc38186bfe1b87353de0e41dacbe8d0f076fcc554'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.2.0/tbls-ask_v0.2.0_linux_arm64.tar.gz'
-      sha256 'cd4706ee623e867c9449883678fd18d3a97eae574543fd6d5bcd46b23104c1a1'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.3.0/tbls-ask_v0.3.0_linux_arm64.tar.gz'
+      sha256 '67d06da2bbc01b782a12b898f35b8d46396d8da1d2f206179c16065098e9b00e'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.2.0/tbls-ask_v0.2.0_linux_amd64.tar.gz'
-      sha256 'b4ce8529ebcdcba204535c9f7d10727d7674079076c8dc99f82450cc66ec0ae2'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.3.0/tbls-ask_v0.3.0_linux_amd64.tar.gz'
+      sha256 '6812493aa6eea413ca79f441995a31db0c494769c6648c36801dabd4849c11c7'
     end
   end
 
--- tbls.rb
+++ tbls.rb
@@ -1,27 +1,27 @@
 class Tbls < Formula
   desc 'tbls is a CI-Friendly tool for document a database, written in Go.'
-  version '1.79.0'
+  version '1.79.2'
   homepage 'https://github.com/k1LoW/tbls'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.79.0/tbls_v1.79.0_darwin_arm64.zip'
-      sha256 '783301f1ad7e71673c94e600ab79c5bfd39189b506926a1ae68e6a70e13e43ad'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.79.2/tbls_v1.79.2_darwin_arm64.zip'
+      sha256 '0a5ff868c00e2fbb7d32e1174053ea7f35339b833b681cbabd800cdb8c5e1f46'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.79.0/tbls_v1.79.0_darwin_amd64.zip'
-      sha256 '99457e4a680440f83acc307076b5d00a6cb2425a572c237cbedb904668e0c824'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.79.2/tbls_v1.79.2_darwin_amd64.zip'
+      sha256 'f9a1f259ea5dc9a09ae57ee2418b61c121fc96a6f32280f6fe14b182d168275e'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.79.0/tbls_v1.79.0_linux_arm64.tar.gz'
-      sha256 '927255b724400bf8fee51089483f83604f91e748cc8f5858f0e37e931640265c'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.79.2/tbls_v1.79.2_linux_arm64.tar.gz'
+      sha256 '6ffe501f564c6d9dbd55f924c75419828539f1f35d9ee439321477ff1ed19c55'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.79.0/tbls_v1.79.0_linux_amd64.tar.gz'
-      sha256 'e49a33f45a3aa09a42382de8dc5e507ada16ef6915e0eaf4b78bf82a0bce5dcb'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.79.2/tbls_v1.79.2_linux_amd64.tar.gz'
+      sha256 'bc274be498dfc9422fb88a66c1fa075ff49501b9d8652c5321dfe4ed561a433b'
     end
   end
 
