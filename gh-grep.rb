b0e07048dcc767f5eea48d2675b77ff2dc81fb47
k1LoW <k1LoW@users.noreply.github.com>
2024-12-12 18:04:40 +0000

    Commit from GitHub Actions (Update formulae)

--- gh-grep.rb
+++ gh-grep.rb
@@ -1,27 +1,27 @@
 class GhGrep < Formula
   desc ':octocat: Print lines matching a pattern in repositories using GitHub API'
-  version '1.2.1'
+  version '1.2.3'
   homepage 'https://github.com/k1LoW/gh-grep'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/gh-grep/releases/download/v1.2.1/gh-grep_v1.2.1_darwin_arm64.zip'
-      sha256 '8ce7e087cbcbe6b922bb34f3431084e1498f07892cb6ee612ff35d126c3b1435'
+      url 'https://github.com/k1LoW/gh-grep/releases/download/v1.2.3/gh-grep_v1.2.3_darwin_arm64.zip'
+      sha256 '87f21688ed0291b4f3946732c91dfbcab451383d2d2f148f5a5004f045e2ff90'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gh-grep/releases/download/v1.2.1/gh-grep_v1.2.1_darwin_amd64.zip'
-      sha256 'a1e0fb06a2db36d1a3aabb777f1c238caa07113477ad67702dd0a228174e0bb6'
+      url 'https://github.com/k1LoW/gh-grep/releases/download/v1.2.3/gh-grep_v1.2.3_darwin_amd64.zip'
+      sha256 '569e34d64cdc6cafbadd04b25354b345ff1d1c12b45bba5f76f791885b27c27f'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/gh-grep/releases/download/v1.2.1/gh-grep_v1.2.1_linux_arm64.tar.gz'
-      sha256 'fd0d7faa5b955b809cdcf04cc9a927eb287231052ed5c1dad3ef238ba35ec4e6'
+      url 'https://github.com/k1LoW/gh-grep/releases/download/v1.2.3/gh-grep_v1.2.3_linux_arm64.tar.gz'
+      sha256 '20a293c07f2e65b0d3fe1ed2f37f24b7130577adcd3b27bcfb2453d5e93d255b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gh-grep/releases/download/v1.2.1/gh-grep_v1.2.1_linux_amd64.tar.gz'
-      sha256 '6b9fc0bd43660537edd8e43206035a4ee26cd47d09ef12d45ce81c3cb78c095e'
+      url 'https://github.com/k1LoW/gh-grep/releases/download/v1.2.3/gh-grep_v1.2.3_linux_amd64.tar.gz'
+      sha256 '391f261e4f17ae8a9c8e3f431eebf14b807b9ab553240ae5c27f973241d809bb'
     end
   end
 
