37b48de8b93912037622c4aa502bfcfc60ec550b
k1LoW <k1LoW@users.noreply.github.com>
2025-01-27 18:04:15 +0000

    Commit from GitHub Actions (Update formulae)

--- tmpmod.rb
+++ tmpmod.rb
@@ -1,27 +1,27 @@
 class Tmpmod < Formula
   desc 'tmpmod is a tool for temporary use of modified modules.'
-  version '0.4.0'
+  version '0.4.1'
   homepage 'https://github.com/k1LoW/tmpmod'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.0/tmpmod_v0.4.0_darwin_arm64.zip'
-      sha256 '3e0a4d6b01e374ff7e366db6080309b1ed5c32ab7f96899c1d797dd8a57feb0e'
+      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.1/tmpmod_v0.4.1_darwin_arm64.zip'
+      sha256 '1ebb7ca43e3db013bc3d6ceafcebe9d201e8a1ec9bbe38ba8a8260e5c9135e51'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.0/tmpmod_v0.4.0_darwin_amd64.zip'
-      sha256 '42b6385cd0e0806cc4632272857ec1388f042957f5dbf87fdf13d2b024eed549'
+      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.1/tmpmod_v0.4.1_darwin_amd64.zip'
+      sha256 '6ebedb3e3af87a44e3aba5744637b64559e36a8beebfa0390aab329a0eceaadd'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.0/tmpmod_v0.4.0_linux_arm64.tar.gz'
-      sha256 'ba742e9e3de1477c363d680fcc1b21c6c7e14057e099bcf57efa4ba5ef74e47c'
+      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.1/tmpmod_v0.4.1_linux_arm64.tar.gz'
+      sha256 '257c822d9405e95714956285e2ab762cb8220a5b0d3284be7460420e0640d4b3'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.0/tmpmod_v0.4.0_linux_amd64.tar.gz'
-      sha256 'abe851005cddd0464ded7f120b3ed98c4a0044e153f35f598de51711b8756521'
+      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.1/tmpmod_v0.4.1_linux_amd64.tar.gz'
+      sha256 'ba36a033f2d085f555f3e8f0cd75b5c11fca429d02906f4583a1fd21def03f00'
     end
   end
 
