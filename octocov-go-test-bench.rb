4e21248e0e572be9ea1ba16114dd19e088536ff3
k1LoW <k1LoW@users.noreply.github.com>
2025-02-01 06:04:06 +0000

    Commit from GitHub Actions (Update formulae)

--- octocov-go-test-bench.rb
+++ octocov-go-test-bench.rb
@@ -1,27 +1,27 @@
 class OctocovGoTestBench < Formula
   desc 'Generate custom metrics JSON from the output of `go test -bench`.'
-  version '1.4.0'
+  version '1.4.1'
   homepage 'https://github.com/k1LoW/octocov-go-test-bench'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.0/octocov-go-test-bench_v1.4.0_darwin_arm64.zip'
-      sha256 'b83e77ff339705e30415f524ca6e81275e2ad97d774e60c20e623ec219ac2e38'
+      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.1/octocov-go-test-bench_v1.4.1_darwin_arm64.zip'
+      sha256 'ea3ec7799dfa74ac7bd17d51f47a412ade5f6469e30f001e8c0d8ed9930c4b07'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.0/octocov-go-test-bench_v1.4.0_darwin_amd64.zip'
-      sha256 'cc548d5ec3f8f8f606a94f92882477ae195b9893d7da4d5ec8f0628b5ea883e5'
+      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.1/octocov-go-test-bench_v1.4.1_darwin_amd64.zip'
+      sha256 'fc71cfa56aab1d65f1af613b9d8ed457fd13d07bbad7bb19f065ac0823864aae'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.0/octocov-go-test-bench_v1.4.0_linux_arm64.tar.gz'
-      sha256 'd835cbb59e4543b245155dff6319dc44ddd6d6805deb4cee19b6b5b58cb9dc1a'
+      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.1/octocov-go-test-bench_v1.4.1_linux_arm64.tar.gz'
+      sha256 '7730357800789276df527fc0ca5d22f2ff42b6f260e40830c6a1eb811f9518d2'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.0/octocov-go-test-bench_v1.4.0_linux_amd64.tar.gz'
-      sha256 'b46351bd168e92ecb90b47d6bc36bebf8eab5cd8883f39475205eb4e1d1f3667'
+      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.1/octocov-go-test-bench_v1.4.1_linux_amd64.tar.gz'
+      sha256 'c568e0605b61c02cf93730458e570dee3385dde63150d718cb6604e64eadc351'
     end
   end
 
