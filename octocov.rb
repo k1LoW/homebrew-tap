1d0a913955a6fe95bd37d2a9f2715477e7ce285c
k1LoW <k1LoW@users.noreply.github.com>
2025-02-03 18:04:31 +0000

    Commit from GitHub Actions (Update formulae)

--- octocov.rb
+++ octocov.rb
@@ -1,24 +1,24 @@
 class Octocov < Formula
   desc 'octocov is a toolkit for collecting code metrics (code coverage, code to test ratio and test execution time).'
-  version '0.62.5'
+  version '0.62.6'
   license "MIT"
   homepage 'https://github.com/k1LoW/octocov'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.62.5/octocov_v0.62.5_darwin_arm64.zip'
-      sha256 '6e9f65dc6146d9d251ed92bebbefd105f8e34301a39711a33af3930744971100'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.62.6/octocov_v0.62.6_darwin_arm64.zip'
+      sha256 '5bf9eb030f92d376d68a424f23bd34c616a3fd44372d2b2aa2a3f764921ee53f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.62.5/octocov_v0.62.5_darwin_amd64.zip'
-      sha256 '9e7d067c17eb2cc3ee1cc7e25e53feb89cc2d703bc9680df268907fe0407c2e6'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.62.6/octocov_v0.62.6_darwin_amd64.zip'
+      sha256 '14678daeb38b4b3e68b4d3a8121503b11bedfbb0d701e9bdd70ef4377692acc6'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.62.5/octocov_v0.62.5_linux_amd64.tar.gz'
-      sha256 '2437d00862e06e3064ff6fa62ebdfe96eebd96976cdeda6b7a8df57dc4e436c6'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.62.6/octocov_v0.62.6_linux_amd64.tar.gz'
+      sha256 'bf73a0bbed1775ba1091f9605f8fdfa4d2bb10d4ac7a56244abf08106392167d'
     end
   end
 
--- tbls-ask.rb
+++ tbls-ask.rb
@@ -1,27 +1,27 @@
 class TblsAsk < Formula
   desc 'tbls-ask is an external subcommand of tbls for asking OpenAI using the datasource.'
-  version '0.3.1'
+  version '0.4.1'
   homepage 'https://github.com/k1LoW/tbls-ask'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.3.1/tbls-ask_v0.3.1_darwin_arm64.zip'
-      sha256 '1583ae4744388d6c7e5fb4f6bcba558c5c441881b3c013690baae3e370880e6e'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.4.1/tbls-ask_v0.4.1_darwin_arm64.zip'
+      sha256 'f64090c24d6dc1fa803eb2946345e4da28c70c77321e183b3e5cdcb60ddab00c'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.3.1/tbls-ask_v0.3.1_darwin_amd64.zip'
-      sha256 '7312849f8f0ea3b090a470ddc79c8d25b9b3dce32dff4b546597e3d85511cb1f'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.4.1/tbls-ask_v0.4.1_darwin_amd64.zip'
+      sha256 '0a927d33d9e9b535b50e8158927dae48708edd0ffd4231a7518db00b28f66f2e'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.3.1/tbls-ask_v0.3.1_linux_arm64.tar.gz'
-      sha256 '1775cff03e8239b33f439e0a3c41e00fee8bfefcc9796486ff264b135966942f'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.4.1/tbls-ask_v0.4.1_linux_arm64.tar.gz'
+      sha256 'f603b80e6b618306ed71fcfa3bf3c694cbcc4b22c86e9f0eb4002da4365bb25d'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.3.1/tbls-ask_v0.3.1_linux_amd64.tar.gz'
-      sha256 '07a556a1b288a65d77e58a18009654e08f941613532118e23734010b7d59ec0c'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.4.1/tbls-ask_v0.4.1_linux_amd64.tar.gz'
+      sha256 '4208bca509d1af82546b2a4e0fe44eba7d36fb55349a621981b2277b2abde458'
     end
   end
 
