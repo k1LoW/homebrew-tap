95ead9be385293d6182bbbc277b8fa96505ce6a9
k1LoW <k1LoW@users.noreply.github.com>
2025-02-21 18:04:22 +0000

    Commit from GitHub Actions (Update formulae)

--- tbls-driver-tailordb.rb
+++ tbls-driver-tailordb.rb
@@ -1,27 +1,27 @@
 class TblsDriverTailordb < Formula
   desc 'tbls driver for TailorDB schema definition'
-  version '0.1.0'
+  version '0.2.0'
   homepage 'https://github.com/k1LoW/tbls-driver-tailordb'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tbls-driver-tailordb/releases/download/v0.1.0/tbls-driver-tailordb_v0.1.0_darwin_arm64.zip'
-      sha256 'c05a426be413c6115d718ac0d5d540d37601e75cda1fb49b559706c83feb853a'
+      url 'https://github.com/k1LoW/tbls-driver-tailordb/releases/download/v0.2.0/tbls-driver-tailordb_v0.2.0_darwin_arm64.zip'
+      sha256 'f37eba308352e41b5fc963988399789d47a7e2ba27e2ba5bef079315fb72731b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls-driver-tailordb/releases/download/v0.1.0/tbls-driver-tailordb_v0.1.0_darwin_amd64.zip'
-      sha256 'e90810082c60292f13e0bcece7f6ef3881370a0c8249c75ce0155a7e86b1dcd6'
+      url 'https://github.com/k1LoW/tbls-driver-tailordb/releases/download/v0.2.0/tbls-driver-tailordb_v0.2.0_darwin_amd64.zip'
+      sha256 'a9af6a9bb61749b589fff381e8eb723c746996f48f05fdbcb29ad3fec4d95e4a'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tbls-driver-tailordb/releases/download/v0.1.0/tbls-driver-tailordb_v0.1.0_linux_arm64.tar.gz'
-      sha256 '124afa30bde0709aac4899d1f68dbb4eb7716a81f857722833225b0d6ab3edee'
+      url 'https://github.com/k1LoW/tbls-driver-tailordb/releases/download/v0.2.0/tbls-driver-tailordb_v0.2.0_linux_arm64.tar.gz'
+      sha256 'd5a4c50499983ba4a4e7931199314aec4bf1bb435c2f92c3bbe7b65c849066b0'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls-driver-tailordb/releases/download/v0.1.0/tbls-driver-tailordb_v0.1.0_linux_amd64.tar.gz'
-      sha256 'c7399ab77f72b8c43829fdaf04339d48a0487e15100ef83f6b6b8ef8de2a5e1b'
+      url 'https://github.com/k1LoW/tbls-driver-tailordb/releases/download/v0.2.0/tbls-driver-tailordb_v0.2.0_linux_amd64.tar.gz'
+      sha256 '36c5f468885a7bd0487fa684ea6d79a6be6438c017e7db2a40455c9d88fae80d'
     end
   end
 
