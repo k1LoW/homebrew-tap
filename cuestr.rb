7931ab5afe8cd6e3b7df7c05d4aaa9b18e016b3e
k1LoW <k1LoW@users.noreply.github.com>
2025-02-25 15:17:35 +0000

    Commit from GitHub Actions (Update formulae)

--- cuestr.rb
+++ cuestr.rb
@@ -1,27 +1,27 @@
 class Cuestr < Formula
   desc 'cuestr is a utility tool for string literals in CUE files.'
-  version '0.0.1'
+  version '0.1.0'
   homepage 'https://github.com/k1LoW/cuestr'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.0.1/cuestr_v0.0.1_darwin_arm64.zip'
-      sha256 '51cdcdbcea777f1afe29ba9207effc151a271440634479f966763d469a4daa3c'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.1.0/cuestr_v0.1.0_darwin_arm64.zip'
+      sha256 '6b5163d9db8a2821a70fe8df5abf86e3b6ba8367dcaf685645a5a8da7a912628'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.0.1/cuestr_v0.0.1_darwin_amd64.zip'
-      sha256 'ffcb84c78862ec0799f510615d8324a90c7b38779e44a00d798acd61adf7ac32'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.1.0/cuestr_v0.1.0_darwin_amd64.zip'
+      sha256 'e5ccd998ae0a861196cb5bfa30c4cc1e5e467447021cb3f63b54877ccf0faab3'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.0.1/cuestr_v0.0.1_linux_arm64.tar.gz'
-      sha256 '6eda5c9f0572b1ce2ec3ce7366b003d3dc15b89a1c1dda2c949529db2cf1752a'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.1.0/cuestr_v0.1.0_linux_arm64.tar.gz'
+      sha256 '98c38749445051116c6928a68b2324c996e5aaa16a53389373d253c04749c4e6'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.0.1/cuestr_v0.0.1_linux_amd64.tar.gz'
-      sha256 '89894578905b6ab9b709dc25e9a8c68699c9580257bb1fb40c390b388c7a3fb9'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.1.0/cuestr_v0.1.0_linux_amd64.tar.gz'
+      sha256 '5f8d2ee22c8775f104cd40e684df13110ad9aa5ee04a9f5a02b784008a55d851'
     end
   end
 
