d0beaa25f379819890b84f94fd0472137fc560cc
k1LoW <k1LoW@users.noreply.github.com>
2025-02-26 00:13:27 +0000

    Commit from GitHub Actions (Update formulae)

--- cuestr.rb
+++ cuestr.rb
@@ -1,27 +1,27 @@
 class Cuestr < Formula
   desc 'cuestr is a utility tool for string literals in CUE files.'
-  version '0.2.1'
+  version '0.3.0'
   homepage 'https://github.com/k1LoW/cuestr'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.2.1/cuestr_v0.2.1_darwin_arm64.zip'
-      sha256 '0ca01996ff5e72a0c62d924076a2c6978d3f801b9bae3d5d6f43c1d19eaef644'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.3.0/cuestr_v0.3.0_darwin_arm64.zip'
+      sha256 '1af2a61709dcf7329070f9b74bb3f64af9a8ab5113559a78388af1af782ee80c'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.2.1/cuestr_v0.2.1_darwin_amd64.zip'
-      sha256 'd4b7c99472349302be22604914edf143d9731681dae755420970958f1e490b06'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.3.0/cuestr_v0.3.0_darwin_amd64.zip'
+      sha256 '26cf630d0eb354553fc784b0d1bf3fba7fa7e9fc4247537ef094ec7d1bf3e2b1'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.2.1/cuestr_v0.2.1_linux_arm64.tar.gz'
-      sha256 'e5e29bcb405033b86505222d51b7c70d6e79e233d1733899f136467b89ebbca8'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.3.0/cuestr_v0.3.0_linux_arm64.tar.gz'
+      sha256 '13a75094be95f261b89e941a8f56338d7e805c7b5fe7a2b6ba6f49c83ce9393a'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.2.1/cuestr_v0.2.1_linux_amd64.tar.gz'
-      sha256 '0c022d2993526a2994337e95dfcdc0e3d8ae61afaf5a01e3110085217ff1d72a'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.3.0/cuestr_v0.3.0_linux_amd64.tar.gz'
+      sha256 '4fc21a8e7ef5b3b36a07dae4d45513b9580c289b74224952faa498fb8528b245'
     end
   end
 
