4bd295743575b7d9097e68530988fad32d3e2e94
k1LoW <k1LoW@users.noreply.github.com>
2025-03-01 18:04:19 +0000

    Commit from GitHub Actions (Update formulae)

--- octocov-go-test-bench.rb
+++ octocov-go-test-bench.rb
@@ -1,27 +1,27 @@
 class OctocovGoTestBench < Formula
   desc 'Generate custom metrics JSON from the output of `go test -bench`.'
-  version '1.4.1'
+  version '1.4.2'
   homepage 'https://github.com/k1LoW/octocov-go-test-bench'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.1/octocov-go-test-bench_v1.4.1_darwin_arm64.zip'
-      sha256 'ea3ec7799dfa74ac7bd17d51f47a412ade5f6469e30f001e8c0d8ed9930c4b07'
+      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.2/octocov-go-test-bench_v1.4.2_darwin_arm64.zip'
+      sha256 '35b059c4165c1ce304abde6aeb08092d4cdb25ad401dc5b799c0887bda2e130e'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.1/octocov-go-test-bench_v1.4.1_darwin_amd64.zip'
-      sha256 'fc71cfa56aab1d65f1af613b9d8ed457fd13d07bbad7bb19f065ac0823864aae'
+      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.2/octocov-go-test-bench_v1.4.2_darwin_amd64.zip'
+      sha256 'a71385d9fea3ba81922026b9b4a98f7970e5b55b340706d0f77db0caf859a826'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.1/octocov-go-test-bench_v1.4.1_linux_arm64.tar.gz'
-      sha256 '7730357800789276df527fc0ca5d22f2ff42b6f260e40830c6a1eb811f9518d2'
+      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.2/octocov-go-test-bench_v1.4.2_linux_arm64.tar.gz'
+      sha256 'd66a64944c94a7513cf9b51d43d2d425ce59bb2d9010d689b41b0fb8677d5e3f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.1/octocov-go-test-bench_v1.4.1_linux_amd64.tar.gz'
-      sha256 'c568e0605b61c02cf93730458e570dee3385dde63150d718cb6604e64eadc351'
+      url 'https://github.com/k1LoW/octocov-go-test-bench/releases/download/v1.4.2/octocov-go-test-bench_v1.4.2_linux_amd64.tar.gz'
+      sha256 '7be34b3b365b0e5acf9eaf6e6e468767932bec65d671db096037a624b7cfc02e'
     end
   end
 
--- tbls.rb
+++ tbls.rb
@@ -1,27 +1,27 @@
 class Tbls < Formula
   desc 'tbls is a CI-Friendly tool for document a database, written in Go.'
-  version '1.81.0'
+  version '1.82.0'
   homepage 'https://github.com/k1LoW/tbls'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.81.0/tbls_v1.81.0_darwin_arm64.zip'
-      sha256 '25346f8ad7750d18368d7f72a0b86ffc72d7be30236f426dca71ea80a0d31754'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.82.0/tbls_v1.82.0_darwin_arm64.zip'
+      sha256 'b061da93dc36517eed2dd24ee1c1398b0151ed79832e896ceaf1cb73bcfee4d5'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.81.0/tbls_v1.81.0_darwin_amd64.zip'
-      sha256 'abece41d7017467387f9a6cc94139d307aef7f2bd6ad5e298aaf1eb7f020e1ef'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.82.0/tbls_v1.82.0_darwin_amd64.zip'
+      sha256 '19e3e05a298f5a824e08a81aa8749d3feb93235c5b99778d69d407079bbd5283'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.81.0/tbls_v1.81.0_linux_arm64.tar.gz'
-      sha256 'fec4285f1e72781b3566548ef63fd40ff67a4ca2ea456b62e76f7526842d7a98'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.82.0/tbls_v1.82.0_linux_arm64.tar.gz'
+      sha256 '0a2c5cce17ddc868db12da2eaf5c0e7f1e92ca8541623a917d536cca50381302'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.81.0/tbls_v1.81.0_linux_amd64.tar.gz'
-      sha256 '55374bf062ff131b8a4d4e102650f4d09e64ee59bd9123469c168806d5ea5986'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.82.0/tbls_v1.82.0_linux_amd64.tar.gz'
+      sha256 '9ba20f525416da530beb5d070bca2c1d1b7addc67f790c77a5992db324e33924'
     end
   end
 
