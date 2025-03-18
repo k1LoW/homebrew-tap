9e468447de92a0c04a4a3d91c42e8d44e69a91b1
k1LoW <k1LoW@users.noreply.github.com>
2025-03-18 12:46:32 +0000

    Commit from GitHub Actions (Update formulae)

--- deck.rb
+++ deck.rb
@@ -1,27 +1,27 @@
 class Deck < Formula
   desc 'deck is a tool for creating deck using Google Slides.'
-  version '0.9.0'
+  version '0.9.1'
   homepage 'https://github.com/k1LoW/deck'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/deck/releases/download/v0.9.0/deck_v0.9.0_darwin_arm64.zip'
-      sha256 '3a5bc4a75ffa0b7a5b5aab0b43c39e074c562275865d3bd50520669d1281b0e7'
+      url 'https://github.com/k1LoW/deck/releases/download/v0.9.1/deck_v0.9.1_darwin_arm64.zip'
+      sha256 '9eb9a09e5782800ee1a2dffc97186c03cbf3570295dbc8844c6de723ffe330f1'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/deck/releases/download/v0.9.0/deck_v0.9.0_darwin_amd64.zip'
-      sha256 '30ff8807854d92f1f87edd6144ab7cb2a86367d6cacd371791aac3c3c1a90fad'
+      url 'https://github.com/k1LoW/deck/releases/download/v0.9.1/deck_v0.9.1_darwin_amd64.zip'
+      sha256 '6613d5d14c24d715d79f8e2c16f12b22ef3d5fe58c4d3ace54de628156a0cd60'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/deck/releases/download/v0.9.0/deck_v0.9.0_linux_arm64.tar.gz'
-      sha256 '0fc6988533694a3536b94de36fd831926449d8e4b744e08d98d50bd6022faaef'
+      url 'https://github.com/k1LoW/deck/releases/download/v0.9.1/deck_v0.9.1_linux_arm64.tar.gz'
+      sha256 '0b78df1a98d7072398693824ac62a7e1db4636e7e50fced5f9eca94b37e909e8'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/deck/releases/download/v0.9.0/deck_v0.9.0_linux_amd64.tar.gz'
-      sha256 'c4967c68078bc5b1393810b52574fac436dcd28e9c8038fcdb512d048a77933f'
+      url 'https://github.com/k1LoW/deck/releases/download/v0.9.1/deck_v0.9.1_linux_amd64.tar.gz'
+      sha256 '7d1eb5705190462de98a13247c15f68dba30b77e4d471abb9184088f87369671'
     end
   end
 
--- tbls.rb
+++ tbls.rb
@@ -1,27 +1,27 @@
 class Tbls < Formula
   desc 'tbls is a CI-Friendly tool for document a database, written in Go.'
-  version '1.83.0'
+  version '1.84.0'
   homepage 'https://github.com/k1LoW/tbls'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.83.0/tbls_v1.83.0_darwin_arm64.zip'
-      sha256 '6446771f742f7ca9830cb0fbdfbe5d81db24567db2e91ee6c7f190459c6e05c8'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.84.0/tbls_v1.84.0_darwin_arm64.zip'
+      sha256 'fab58a2437857da29a0fc3e2565b644b763eaa4df92bdf8ecd206b29591a5d85'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.83.0/tbls_v1.83.0_darwin_amd64.zip'
-      sha256 '77e488f8015e06ee1d745d0991d9eaa99d89d406c844a9a8f1f9a3dbf8ac8211'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.84.0/tbls_v1.84.0_darwin_amd64.zip'
+      sha256 'e7e8fe6161f3692fdcb26ab7220904df8897a35a422490332a14e3f10d6e0893'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.83.0/tbls_v1.83.0_linux_arm64.tar.gz'
-      sha256 'f4441abd1875a5b3d556708fddbc1badd9149691ad8f876d662868f63699fe64'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.84.0/tbls_v1.84.0_linux_arm64.tar.gz'
+      sha256 '343acd06769e3510cc02b0c09cc3853c89822ce7d845d2b031cdadb2acd13060'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.83.0/tbls_v1.83.0_linux_amd64.tar.gz'
-      sha256 '6a41607d79a8fd59154d861d9d623055d8ffe51f262675a8525272c2e639b9fb'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.84.0/tbls_v1.84.0_linux_amd64.tar.gz'
+      sha256 '83f35a07fd2a00c2aa360a47edca6d261f5208186911977eff39097151fc57d5'
     end
   end
 
