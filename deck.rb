4859e3d35fa9437396d5e49892f4086b0a6792fc
k1LoW <k1LoW@users.noreply.github.com>
2025-03-19 11:50:52 +0000

    Commit from GitHub Actions (Update formulae)

--- deck.rb
+++ deck.rb
@@ -1,27 +1,27 @@
 class Deck < Formula
   desc 'deck is a tool for creating deck using Google Slides.'
-  version '0.9.1'
+  version '0.10.0'
   homepage 'https://github.com/k1LoW/deck'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/deck/releases/download/v0.9.1/deck_v0.9.1_darwin_arm64.zip'
-      sha256 '9eb9a09e5782800ee1a2dffc97186c03cbf3570295dbc8844c6de723ffe330f1'
+      url 'https://github.com/k1LoW/deck/releases/download/v0.10.0/deck_v0.10.0_darwin_arm64.zip'
+      sha256 '31537ef2dbaf88b8b9c269332248363fd5677be977041c506ccdf7aa6f8073a0'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/deck/releases/download/v0.9.1/deck_v0.9.1_darwin_amd64.zip'
-      sha256 '6613d5d14c24d715d79f8e2c16f12b22ef3d5fe58c4d3ace54de628156a0cd60'
+      url 'https://github.com/k1LoW/deck/releases/download/v0.10.0/deck_v0.10.0_darwin_amd64.zip'
+      sha256 '51fd80dbc2ee13a7be584a47eead8d586a2a7e0a3348ef2b7d7440dc21f28717'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/deck/releases/download/v0.9.1/deck_v0.9.1_linux_arm64.tar.gz'
-      sha256 '0b78df1a98d7072398693824ac62a7e1db4636e7e50fced5f9eca94b37e909e8'
+      url 'https://github.com/k1LoW/deck/releases/download/v0.10.0/deck_v0.10.0_linux_arm64.tar.gz'
+      sha256 'a5c91d3fc7d3d5f72fd39d82750d5ae8deeca2d130c55d385ff448bce143cf47'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/deck/releases/download/v0.9.1/deck_v0.9.1_linux_amd64.tar.gz'
-      sha256 '7d1eb5705190462de98a13247c15f68dba30b77e4d471abb9184088f87369671'
+      url 'https://github.com/k1LoW/deck/releases/download/v0.10.0/deck_v0.10.0_linux_amd64.tar.gz'
+      sha256 '4019a34c3348fbdecbafc08afa717d8187f016ba0fc8af791d93aead8c8f98fc'
     end
   end
 
--- gh-setup.rb
+++ gh-setup.rb
@@ -1,24 +1,24 @@
 class GhSetup < Formula
   desc ':octocat: Setup asset of Github releases.'
-  version '1.9.2'
+  version '1.10.0'
   homepage 'https://github.com/k1LoW/gh-setup'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.9.2/gh-setup_v1.9.2_darwin_arm64.zip'
-      sha256 '1846744917f18029b551e89fcf3bfc07d8229aac8e9977105b2f26a60e66cecd'
+      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.10.0/gh-setup_v1.10.0_darwin_arm64.zip'
+      sha256 '534cd002dfd7332124fef313dfddfa8f3fd960c4f7a4629c962a110c866f1110'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.9.2/gh-setup_v1.9.2_darwin_amd64.zip'
-      sha256 '85ccbb6263f4961318c8920912c3ff940965d943ad547e077fc25578bb1657a2'
+      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.10.0/gh-setup_v1.10.0_darwin_amd64.zip'
+      sha256 '8647f35cdd125050a94b9d184757393f971aa612c8fb364e3b8f3405488aa61a'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.9.2/gh-setup_v1.9.2_linux_amd64.tar.gz'
-      sha256 '134d04df45860f2b859c020583b27989b6ad8a8144bd534a5536a9c405bafaee'
+      url 'https://github.com/k1LoW/gh-setup/releases/download/v1.10.0/gh-setup_v1.10.0_linux_amd64.tar.gz'
+      sha256 '1b6812a8634311833a4a62da27f13efe4a42d04776941b31a6f2c73f5c05f46a'
     end
   end
 
