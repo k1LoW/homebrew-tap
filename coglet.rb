cced8f9f228fcc6ed057c32c699e24793b400b30
k1LoW <k1LoW@users.noreply.github.com>
2025-04-01 05:09:05 +0000

    Commit from GitHub Actions (Update formulae)

--- coglet.rb
+++ coglet.rb
@@ -1,27 +1,27 @@
 class Coglet < Formula
   desc 'coglet is a tool for User pool of Amazon Cognito.'
-  version '0.1.2'
+  version '0.1.3'
   homepage 'https://github.com/k1LoW/coglet'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/coglet/releases/download/v0.1.2/coglet_v0.1.2_darwin_arm64.zip'
-      sha256 '2379046f0c4417a4b73b80ed8e22cd89b3911b8d32c9982672fdf105cec175fb'
+      url 'https://github.com/k1LoW/coglet/releases/download/v0.1.3/coglet_v0.1.3_darwin_arm64.zip'
+      sha256 'ec3bab8a035f75af1ab84e23f7a187c24fcaad341238acf00e9e003174f1ffb1'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/coglet/releases/download/v0.1.2/coglet_v0.1.2_darwin_amd64.zip'
-      sha256 'aebf1ae2626d9112c685d48beaee0cf239bf74e111f4854bbba9624c03908758'
+      url 'https://github.com/k1LoW/coglet/releases/download/v0.1.3/coglet_v0.1.3_darwin_amd64.zip'
+      sha256 'e3efaa87f0fc42b8837d174f492d1a92bb5900687e733156a6260487c18251b5'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/coglet/releases/download/v0.1.2/coglet_v0.1.2_linux_arm64.tar.gz'
-      sha256 '077488de12a9a8af271fa4da84138c4075ba48d8a4dccd5ca09b0a0eed072b5a'
+      url 'https://github.com/k1LoW/coglet/releases/download/v0.1.3/coglet_v0.1.3_linux_arm64.tar.gz'
+      sha256 '7912a0c11c83da8ff6357d3e6a9e52d1c4eb1f5be9914ce3110663bd8f633335'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/coglet/releases/download/v0.1.2/coglet_v0.1.2_linux_amd64.tar.gz'
-      sha256 '273485c688a3822a68fb3cf01f22023c879c000c64859af2d6fa2d71b1bc4ea0'
+      url 'https://github.com/k1LoW/coglet/releases/download/v0.1.3/coglet_v0.1.3_linux_amd64.tar.gz'
+      sha256 '63df12fcb577ac7d84fe56de391dd763d3891e7738b7e1db79d86eeeb09c1884'
     end
   end
 
