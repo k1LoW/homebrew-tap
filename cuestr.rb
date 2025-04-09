73f592980678d2ac4ecb16aa625a5ab1f59073f4
k1LoW <k1LoW@users.noreply.github.com>
2025-04-09 18:04:40 +0000

    Commit from GitHub Actions (Update formulae)

--- cuestr.rb
+++ cuestr.rb
@@ -1,27 +1,27 @@
 class Cuestr < Formula
   desc 'cuestr is a utility tool for string literals in CUE files.'
-  version '0.4.0'
+  version '0.4.1'
   homepage 'https://github.com/k1LoW/cuestr'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.0/cuestr_v0.4.0_darwin_arm64.zip'
-      sha256 '895d30d83a59cbda7a654454eed1d5159402c526e9c3a2108b836bc9d485c5cd'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.1/cuestr_v0.4.1_darwin_arm64.zip'
+      sha256 '40504d53d612a9c2e718a6b6f2455225f067e65cf13d8b0a9b1588e2df99744f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.0/cuestr_v0.4.0_darwin_amd64.zip'
-      sha256 'ac6472e9f6aec3e3641750e9f0b39a8dd9e00080c3e2107bbe02b1737c312bbc'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.1/cuestr_v0.4.1_darwin_amd64.zip'
+      sha256 '0b8fef93cd2ba2208642c3c3cc294ebf38d0bec905ea66bb0a111eadefa090ab'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.0/cuestr_v0.4.0_linux_arm64.tar.gz'
-      sha256 'b52e9b6e499ed493a4ad35ab779accd2f0bbd2a3576635ee90dbb6347029eef7'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.1/cuestr_v0.4.1_linux_arm64.tar.gz'
+      sha256 'c56a41155189832796f8bf54474cce1fc9152c02e20320361c6fc02f4ca8cad6'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.0/cuestr_v0.4.0_linux_amd64.tar.gz'
-      sha256 '3cb21b118278371fa1d64106e35d0bbd0f38bd29ffef8d93d34dc5e7a1463d2b'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.1/cuestr_v0.4.1_linux_amd64.tar.gz'
+      sha256 'c4b71e0f366177dab44d00d56d5769ddb8ccc82ab21b91ecedbbed8974bcd171'
     end
   end
 
--- runn.rb
+++ runn.rb
@@ -1,24 +1,24 @@
 class Runn < Formula
   desc 'runn is a tool for running operations following a scenario.'
   homepage 'https://github.com/k1LoW/runn'
-  version '0.129.2'
+  version '0.129.3'
   license 'MIT'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/runn/releases/download/v0.129.2/runn_v0.129.2_darwin_arm64.zip'
-      sha256 '110a7e149c32ee69a883cf94d2736b0cd80371e316a4a931e3d12b0d1f1ae6fe'
+      url 'https://github.com/k1LoW/runn/releases/download/v0.129.3/runn_v0.129.3_darwin_arm64.zip'
+      sha256 '86cacff073cf691ef6854ff0d6572710df10f42ed5b7bc05ed6a566be20b275b'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/runn/releases/download/v0.129.2/runn_v0.129.2_darwin_amd64.zip'
-      sha256 'd960d8ca39f748f4542bb70a27dc78d373ce2901f513fa1816eefea8946f5b4e'
+      url 'https://github.com/k1LoW/runn/releases/download/v0.129.3/runn_v0.129.3_darwin_amd64.zip'
+      sha256 '97eee8fac367616adc9a196c1745343fc8dd91dea68bde44a5d7f40c1f45133d'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/runn/releases/download/v0.129.2/runn_v0.129.2_linux_amd64.tar.gz'
-      sha256 '95486205b4e6660dde0aca98e59e9dd73ed35232830b74666c3d1a48f60b211f'
+      url 'https://github.com/k1LoW/runn/releases/download/v0.129.3/runn_v0.129.3_linux_amd64.tar.gz'
+      sha256 'ae4ac73d271badd0a743d0a1207beda2fc19d35a830582ec1ce13f4d7547c3a1'
     end
   end
 
