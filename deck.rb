6d5bb07279391103c07373fcf10e5c6a5cd35112
k1LoW <k1LoW@users.noreply.github.com>
2025-04-22 18:04:33 +0000

    Commit from GitHub Actions (Update formulae)

--- deck.rb
+++ deck.rb
@@ -1,27 +1,27 @@
 class Deck < Formula
   desc 'deck is a tool for creating deck using Google Slides.'
-  version '0.13.3'
+  version '0.13.4'
   homepage 'https://github.com/k1LoW/deck'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/deck/releases/download/v0.13.3/deck_v0.13.3_darwin_arm64.zip'
-      sha256 '97b24fda5e1fbe3a0437d12f85d20daa1252fe2aee0595c5831c547ecc9d5668'
+      url 'https://github.com/k1LoW/deck/releases/download/v0.13.4/deck_v0.13.4_darwin_arm64.zip'
+      sha256 '25b1a203713243be5eab034ac76c9799319b7aabdf8fe0d36ce9b04a90b8ba34'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/deck/releases/download/v0.13.3/deck_v0.13.3_darwin_amd64.zip'
-      sha256 '9a2b6672d925fbf4c1f4ffb2c80b9296018d102e510cb1aea82b4c682ec2ea9b'
+      url 'https://github.com/k1LoW/deck/releases/download/v0.13.4/deck_v0.13.4_darwin_amd64.zip'
+      sha256 'd505b6f9a427fe308234b5a5db72132b7f2fe3a0573805564068530e68df6191'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/deck/releases/download/v0.13.3/deck_v0.13.3_linux_arm64.tar.gz'
-      sha256 '0aa269fe40cce9c77c6cfa506112c182cd18f345cb6f0cdfeaa9203b65ab3fce'
+      url 'https://github.com/k1LoW/deck/releases/download/v0.13.4/deck_v0.13.4_linux_arm64.tar.gz'
+      sha256 '2b172a90155ea59d950c3ceb7d420f6871c7f87e3a486f83673ab7dfc8911643'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/deck/releases/download/v0.13.3/deck_v0.13.3_linux_amd64.tar.gz'
-      sha256 '8d0e7504551ee54893cf946f9ddba03057e2a4f2fa9c6beef05bea827e132c09'
+      url 'https://github.com/k1LoW/deck/releases/download/v0.13.4/deck_v0.13.4_linux_amd64.tar.gz'
+      sha256 '35967f1ce0153eecf83ba409cc0735d2e8f54db49e3a2c178ce937e218d045ed'
     end
   end
 
--- tmpmod.rb
+++ tmpmod.rb
@@ -1,27 +1,27 @@
 class Tmpmod < Formula
   desc 'tmpmod is a tool for temporary use of modified modules.'
-  version '0.4.2'
+  version '0.4.3'
   homepage 'https://github.com/k1LoW/tmpmod'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.2/tmpmod_v0.4.2_darwin_arm64.zip'
-      sha256 '97618e5870255d014f3e835b1e58762a3c876c89f53eb34933bccd8c4fc3de65'
+      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.3/tmpmod_v0.4.3_darwin_arm64.zip'
+      sha256 '27da62327ce29683383dc29155c8a92f8572eb764aab18ebf6a35fa59ae3c944'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.2/tmpmod_v0.4.2_darwin_amd64.zip'
-      sha256 '46a45e5d99fc781728d69a857256bd5f2a8de0344952d68c095cb38171893354'
+      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.3/tmpmod_v0.4.3_darwin_amd64.zip'
+      sha256 'aaa3aed77e1323828c5e6b670595129e590ae3fbedb90da43271b45904ad3b26'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.2/tmpmod_v0.4.2_linux_arm64.tar.gz'
-      sha256 '3356576c7e619264ee5d38c1e9572d6f3a186a44c715972a5d92ab249f2d6f8d'
+      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.3/tmpmod_v0.4.3_linux_arm64.tar.gz'
+      sha256 'f90e1e3d4309fd8bdde609ba5febe5e7b1e7d97732fed3d9af08e3b60dbd39a3'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.2/tmpmod_v0.4.2_linux_amd64.tar.gz'
-      sha256 '70b09d435e85fc3b6143afe943d7e77a2c0f0710bb7592cd5770b34d75d86a29'
+      url 'https://github.com/k1LoW/tmpmod/releases/download/v0.4.3/tmpmod_v0.4.3_linux_amd64.tar.gz'
+      sha256 '2d17adcdccaa9319d09159e2ed78215a4cfa86ed3fc6796fe90f224f2abfe934'
     end
   end
 
