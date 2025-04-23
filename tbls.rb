d8ff12da4ccd987ebda1a253ea9c41b56922a7de
k1LoW <k1LoW@users.noreply.github.com>
2025-04-23 06:04:57 +0000

    Commit from GitHub Actions (Update formulae)

--- tbls.rb
+++ tbls.rb
@@ -1,27 +1,27 @@
 class Tbls < Formula
   desc 'tbls is a CI-Friendly tool for document a database, written in Go.'
-  version '1.85.0'
+  version '1.85.1'
   homepage 'https://github.com/k1LoW/tbls'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.85.0/tbls_v1.85.0_darwin_arm64.zip'
-      sha256 '4c405ae2ffe77ac133320f4cb0a0403c64f9fda90402fa42003d2c202df37b86'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.85.1/tbls_v1.85.1_darwin_arm64.zip'
+      sha256 '2f7b87065652f91c60b20d6c6ccbac3a0281c1cbcf87ed14c953e05ca2e9e630'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.85.0/tbls_v1.85.0_darwin_amd64.zip'
-      sha256 'df31d52d7198744237ff24d0b9419b182827938e69537c42a2bdca28eb697d87'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.85.1/tbls_v1.85.1_darwin_amd64.zip'
+      sha256 'a49ee376b0ae4be6bf31f125f9f085e8d5be4fb88360a0925fa9422e6277bbe0'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.85.0/tbls_v1.85.0_linux_arm64.tar.gz'
-      sha256 'da4672968639105b5ced25068717d73ed8d4399d1ca774c1cb5ab283f9aa92bb'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.85.1/tbls_v1.85.1_linux_arm64.tar.gz'
+      sha256 '186a54965bbc90052c42d27be7101c97d206181797397d68feff036000580e74'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.85.0/tbls_v1.85.0_linux_amd64.tar.gz'
-      sha256 '911af9c05562481c4c93fc361dfda026e43eaa5e701364621efd339c2e1a6ef2'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.85.1/tbls_v1.85.1_linux_amd64.tar.gz'
+      sha256 'b0bb64953c50dcb773b0249a1e59d9eb474caebb7cb9239a71d3c9a9adef289d'
     end
   end
 
