09df13a56a745205b874a96e306c212eb891700e
k1LoW <k1LoW@users.noreply.github.com>
2025-05-14 18:04:38 +0000

    Commit from GitHub Actions (Update formulae)

--- roots.rb
+++ roots.rb
@@ -1,26 +1,26 @@
 class Roots < Formula
-  version '0.3.0'
+  version '0.3.1'
   homepage 'https://github.com/k1LoW/roots'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/roots/releases/download/v0.3.0/roots_v0.3.0_darwin_arm64.zip'
-      sha256 '6ca9cda82874895146f2a101b797d90df623bf5d2f101090215491d37c9f127e'
+      url 'https://github.com/k1LoW/roots/releases/download/v0.3.1/roots_v0.3.1_darwin_arm64.zip'
+      sha256 'fb202f42f52314a5e0f0394331bcab40f31168f95524fa06181c4b5d2794b823'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/roots/releases/download/v0.3.0/roots_v0.3.0_darwin_amd64.zip'
-      sha256 'c5695a3e0b7a0571877cd4fd2983b7e04847f29e8e1756dfe313fb15dad5baa2'
+      url 'https://github.com/k1LoW/roots/releases/download/v0.3.1/roots_v0.3.1_darwin_amd64.zip'
+      sha256 '6b55b5e230bbc0af61ef8c512e8d464ca0c86a2b202bd7112678855aca9a68b8'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/roots/releases/download/v0.3.0/roots_v0.3.0_linux_arm64.tar.gz'
-      sha256 '3839075fdca0d1642413d199c0a369276a2c2f3fe2d5433e5212d8d4df214781'
+      url 'https://github.com/k1LoW/roots/releases/download/v0.3.1/roots_v0.3.1_linux_arm64.tar.gz'
+      sha256 'ef5560f932e33edd79a4c77ccc39f85a6edc2ec0a7737e532dcafc9aa8607969'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/roots/releases/download/v0.3.0/roots_v0.3.0_linux_amd64.tar.gz'
-      sha256 '79cba67a5dcf905f6567bf6201f3ee53b17496bd1d29ded91ab3c87f86d8ee03'
+      url 'https://github.com/k1LoW/roots/releases/download/v0.3.1/roots_v0.3.1_linux_amd64.tar.gz'
+      sha256 '75b317b6f0fe00781a4af3b40fe5e4e28efa9b41edaf14de369f0a9870de4dc7'
     end
   end
 
