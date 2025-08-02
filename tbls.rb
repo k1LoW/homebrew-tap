2496cc3e336a0e2cdcde5a9ab3b441abde2535ea
k1LoW <k1LoW@users.noreply.github.com>
2025-08-02 18:04:33 +0000

    Commit from GitHub Actions (Update formulae)

--- tbls.rb
+++ tbls.rb
@@ -1,29 +1,29 @@
 class Tbls < Formula
   desc 'tbls is a CI-Friendly tool for document a database, written in Go.'
-  version '1.86.1'
+  version '1.87.0'
   homepage 'https://github.com/k1LoW/tbls'
 
   deprecate! date: "2025-08-02", because: "please use the official Homebrew formula instead"
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.86.1/tbls_v1.86.1_darwin_arm64.zip'
-      sha256 '6ea220b13db9a35afa1fd14f8708213f272c2f27917cbcc9412a38799e3d6ff0'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.87.0/tbls_v1.87.0_darwin_arm64.zip'
+      sha256 '5fd061f793723fc461106d89c4ba609d21e47bd07fb4f8320e797eeb33eef729'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.86.1/tbls_v1.86.1_darwin_amd64.zip'
-      sha256 '27687ae8772e1e80bd064c919bf3c56b16a665ff1b8c45a6953593413aec89b2'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.87.0/tbls_v1.87.0_darwin_amd64.zip'
+      sha256 '120bd026cba40521fba3bf0569b7b6460b0d360940735bd89d71212bcc8c12fb'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.86.1/tbls_v1.86.1_linux_arm64.tar.gz'
-      sha256 '93c36a4e12b598f06b6601325fa0a8dcfde8bb8c2b5a34626bbb36754ea786a5'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.87.0/tbls_v1.87.0_linux_arm64.tar.gz'
+      sha256 '27d8eccae81a10e658f4f2ee459025ae0bdb7a2dc794494ab0cf398f316562d1'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.86.1/tbls_v1.86.1_linux_amd64.tar.gz'
-      sha256 'c11b18f38dd5d7cd5c540b4cdf3db4e4b21a7f03b5c73cddfdfab5e9acefc198'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.87.0/tbls_v1.87.0_linux_amd64.tar.gz'
+      sha256 '26546fb40be69c30c1e87d0bd6e15ce1e15c77e426e6bb6b2106faf99dedbe40'
     end
   end
 
