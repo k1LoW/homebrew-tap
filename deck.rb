7b4fff9b46815468e63bf7e782ee55a095a7c1ca
k1LoW <k1LoW@users.noreply.github.com>
2025-08-14 03:25:14 +0000

    Commit from GitHub Actions (Update formulae)

--- deck.rb
+++ deck.rb
@@ -1,29 +1,29 @@
 class Deck < Formula
   desc 'deck is a tool for creating deck using Google Slides.'
-  version '1.13.0'
+  version '1.13.1'
   homepage 'https://github.com/k1LoW/deck'
 
   deprecate! date: "2025-08-02", because: "please use the official Homebrew formula instead"
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/deck/releases/download/v1.13.0/deck_v1.13.0_darwin_arm64.zip'
-      sha256 '5e123d01ae47a9382c8e3e636f1874e29fb4ab39fa6f1a295e978c1daacf20ef'
+      url 'https://github.com/k1LoW/deck/releases/download/v1.13.1/deck_v1.13.1_darwin_arm64.zip'
+      sha256 'c1870bbb31be04c6656ae366155566b1cbaaffdcde39203d047df99921bf86b3'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/deck/releases/download/v1.13.0/deck_v1.13.0_darwin_amd64.zip'
-      sha256 '2562ee0b8d1b5d8b68258a7c8b03631066ee86e3250006422d932c0728b27b54'
+      url 'https://github.com/k1LoW/deck/releases/download/v1.13.1/deck_v1.13.1_darwin_amd64.zip'
+      sha256 'b4b2927086bf06cc7c7c4927781c507def5e735609153fcf38eb4175aee24e4b'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/deck/releases/download/v1.13.0/deck_v1.13.0_linux_arm64.tar.gz'
-      sha256 '15c711289bd7669b0e988b2de24886df8d67318f1137a51dd126e8cdfc0036f8'
+      url 'https://github.com/k1LoW/deck/releases/download/v1.13.1/deck_v1.13.1_linux_arm64.tar.gz'
+      sha256 '52406e37e4268c845d112aee1d8f8f29960acf6618b4560388269c2825c7c577'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/deck/releases/download/v1.13.0/deck_v1.13.0_linux_amd64.tar.gz'
-      sha256 'ff12a860816d6b4998edd2b2b9adf454a80e50000d471fa13a4a14f1d5dd801e'
+      url 'https://github.com/k1LoW/deck/releases/download/v1.13.1/deck_v1.13.1_linux_amd64.tar.gz'
+      sha256 'e942de01b83444fee3ead3d08226ab5cdfab5adde1517f346851b14234ecaad9'
     end
   end
 
--- envdo.rb
+++ envdo.rb
@@ -1,26 +1,26 @@
 class Envdo < Formula
-  version '0.1.0'
+  version '0.2.0'
   homepage 'https://github.com/k1LoW/envdo'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/envdo/releases/download/v0.1.0/envdo_v0.1.0_darwin_arm64.zip'
-      sha256 '04621a3c1a16938e0e29c11a933d8061b210b875852dfc2dbd68071223573caa'
+      url 'https://github.com/k1LoW/envdo/releases/download/v0.2.0/envdo_v0.2.0_darwin_arm64.zip'
+      sha256 'f96e7998b0c2e69a702c9df4d669a31121c49d76a1e64554ae709ee45f42f988'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/envdo/releases/download/v0.1.0/envdo_v0.1.0_darwin_amd64.zip'
-      sha256 '7f43611614e9d604244272c70014d84124da2279a54398c863802576e37ce494'
+      url 'https://github.com/k1LoW/envdo/releases/download/v0.2.0/envdo_v0.2.0_darwin_amd64.zip'
+      sha256 '78a6d444454bbef55e05881b58d475f33ae2e3b5da7906e29a03f89869a1f9d5'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/envdo/releases/download/v0.1.0/envdo_v0.1.0_linux_arm64.tar.gz'
-      sha256 'bf2321489f6601e2d9eedb4864fbc96c8f85adef6550632bd6eeba831c749606'
+      url 'https://github.com/k1LoW/envdo/releases/download/v0.2.0/envdo_v0.2.0_linux_arm64.tar.gz'
+      sha256 'd9402c11060a10a9a802101510dc05ad5b380cf53d1a614e1eba96438cd0e138'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/envdo/releases/download/v0.1.0/envdo_v0.1.0_linux_amd64.tar.gz'
-      sha256 '75f95fde1067553dbbc3c8c4b3e243f3ea31a4e057d52093bbc0076d628819ed'
+      url 'https://github.com/k1LoW/envdo/releases/download/v0.2.0/envdo_v0.2.0_linux_amd64.tar.gz'
+      sha256 '69b82b73f918972a881b29f2ca4232909197a32d950c37c2a1dc3a57c4f07258'
     end
   end
 
