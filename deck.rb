7a91f4a7ffcdd51e583eaa4a6364790b1f9091b6
k1LoW <k1LoW@users.noreply.github.com>
2025-08-16 18:04:25 +0000

    Commit from GitHub Actions (Update formulae)

--- deck.rb
+++ deck.rb
@@ -1,29 +1,29 @@
 class Deck < Formula
   desc 'deck is a tool for creating deck using Google Slides.'
-  version '1.14.0'
+  version '1.15.0'
   homepage 'https://github.com/k1LoW/deck'
 
   deprecate! date: "2025-08-02", because: "please use the official Homebrew formula instead"
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/deck/releases/download/v1.14.0/deck_v1.14.0_darwin_arm64.zip'
-      sha256 'e581b839a712d437aa13d01e4c5712a4400ffcf84687bfd3d0284d41258c7d27'
+      url 'https://github.com/k1LoW/deck/releases/download/v1.15.0/deck_v1.15.0_darwin_arm64.zip'
+      sha256 'b43fdee6a163acac92ba966adf30d1c5711463545a9d32b1072a41e15aabcb5f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/deck/releases/download/v1.14.0/deck_v1.14.0_darwin_amd64.zip'
-      sha256 '2edb08995dfd99828a53ed40e75483f56fa6a4aaf591001617ddd8ecc70c8821'
+      url 'https://github.com/k1LoW/deck/releases/download/v1.15.0/deck_v1.15.0_darwin_amd64.zip'
+      sha256 '7dc7150a6286b8051602c8a34410225879611decc73bf8822d7e5b201117dc80'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/deck/releases/download/v1.14.0/deck_v1.14.0_linux_arm64.tar.gz'
-      sha256 '94a9b403e21fa762b628b79eed7aeb95e5fe055943e2f6a91650118a4a87bb63'
+      url 'https://github.com/k1LoW/deck/releases/download/v1.15.0/deck_v1.15.0_linux_arm64.tar.gz'
+      sha256 'a4034fae3faa1463f87a7f5830c241170c05b56b2aa9058e5ca3b6a61f3b9b75'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/deck/releases/download/v1.14.0/deck_v1.14.0_linux_amd64.tar.gz'
-      sha256 '371cf45fc142e41321e4ffb656626a618d10fa75056ea2d7fd9c7b9d8981f474'
+      url 'https://github.com/k1LoW/deck/releases/download/v1.15.0/deck_v1.15.0_linux_amd64.tar.gz'
+      sha256 '0044d6b6ab9992297974c99d162faf75ddfe6f9836cc72919ed245ec946508a5'
     end
   end
 
