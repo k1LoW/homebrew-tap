ecb94fecc1c10b6bf85ec5af13d5ad9ab96188e6
k1LoW <k1LoW@users.noreply.github.com>
2025-08-28 18:04:36 +0000

    Commit from GitHub Actions (Update formulae)

--- tbls-ask.rb
+++ tbls-ask.rb
@@ -1,27 +1,27 @@
 class TblsAsk < Formula
   desc 'tbls-ask is an external subcommand of tbls for asking OpenAI using the datasource.'
-  version '0.6.2'
+  version '0.6.3'
   homepage 'https://github.com/k1LoW/tbls-ask'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.6.2/tbls-ask_v0.6.2_darwin_arm64.zip'
-      sha256 '2793edfea46409102ea08607a381364c21d4d8ac86c5879a3f56fcf16674ffec'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.6.3/tbls-ask_v0.6.3_darwin_arm64.zip'
+      sha256 '0a689a0195358d9f45649b7f1aa2ee1b3778f1da8c936f18ebe10f5cb534655c'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.6.2/tbls-ask_v0.6.2_darwin_amd64.zip'
-      sha256 'c4c7c286e9ef494c0f7d3c8eb2c2f628567577683dc20883f72244cd2f8c588f'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.6.3/tbls-ask_v0.6.3_darwin_amd64.zip'
+      sha256 '4940fac2ac6c082e0990e7c4324cbdc8faf53ef24200f6348590f4590d77805d'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.6.2/tbls-ask_v0.6.2_linux_arm64.tar.gz'
-      sha256 '9c8dd5454a9cedaba323bacc1cfacd42ab48c041af6511f81d880d1447c61c60'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.6.3/tbls-ask_v0.6.3_linux_arm64.tar.gz'
+      sha256 'efd33323b0dd88156ddc75c4a30b61197a61a72d75c3665655ef76c12e62e8d4'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.6.2/tbls-ask_v0.6.2_linux_amd64.tar.gz'
-      sha256 'bc975b48b997f5cc45047c05e818b5f315f3fab26de4839f539d1dc944b72367'
+      url 'https://github.com/k1LoW/tbls-ask/releases/download/v0.6.3/tbls-ask_v0.6.3_linux_amd64.tar.gz'
+      sha256 'ce5f76d1b8b21808929c25093fa84644e704e4cb75c1bcb9be27f6fd930799fa'
     end
   end
 
