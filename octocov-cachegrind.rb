90b71c8da06f894f8ddc348f8769c52e04156880
k1LoW <k1LoW@users.noreply.github.com>
2025-10-24 18:05:15 +0000

    Commit from GitHub Actions (Update formulae)

--- octocov-cachegrind.rb
+++ octocov-cachegrind.rb
@@ -1,27 +1,27 @@
 class OctocovCachegrind < Formula
   desc 'Generate octocov custom metrics JSON from the output of `valgrind --tool=cachegrind`.'
-  version '0.1.2'
+  version '0.2.1'
   homepage 'https://github.com/k1LoW/octocov-cachegrind'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.1.2/octocov-cachegrind_v0.1.2_darwin_arm64.zip'
-      sha256 '19550f20a6270fd8d9e5c6cbd48adc0f915e880d0e3a6163b833554759779f4c'
+      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.2.1/octocov-cachegrind_v0.2.1_darwin_arm64.zip'
+      sha256 '118d112ae790a2ecf869c15d40b19140b5e078f6f0b11ed4617408dd148baf2e'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.1.2/octocov-cachegrind_v0.1.2_darwin_amd64.zip'
-      sha256 '2d665aa2abf82088c183deff5da7b10c9500005c8e89d64c7c94278edf85481f'
+      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.2.1/octocov-cachegrind_v0.2.1_darwin_amd64.zip'
+      sha256 '69ec893c7f685707b786ea5da68c0581db70d098e48a6f1dfd7cdd3bc4913179'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.1.2/octocov-cachegrind_v0.1.2_linux_arm64.tar.gz'
-      sha256 '5afdd5331be9396f8ad13b261de63f7d3f7d6a3ec8dab7ec620f0524ff36162e'
+      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.2.1/octocov-cachegrind_v0.2.1_linux_arm64.tar.gz'
+      sha256 'ab52699f59365e0995012737f0733a462e4b9e0d987c68615d42f47ec7abf6a6'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.1.2/octocov-cachegrind_v0.1.2_linux_amd64.tar.gz'
-      sha256 '3ae8545d5a9911f5c50b0e5feb0bfd3598a650c3d99c460ddf61d51410d59d15'
+      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.2.1/octocov-cachegrind_v0.2.1_linux_amd64.tar.gz'
+      sha256 '216f7b4dcb95ab642e9c9c55b0d7eabfce8df24941097510657cb968659d3a24'
     end
   end
 
