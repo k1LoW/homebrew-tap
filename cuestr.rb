3a3d9db190189e1b9a57ba8bc6d3abce61b80b7b
k1LoW <k1LoW@users.noreply.github.com>
2025-12-16 06:05:47 +0000

    Commit from GitHub Actions (Update formulae)

--- cuestr.rb
+++ cuestr.rb
@@ -1,27 +1,27 @@
 class Cuestr < Formula
   desc 'cuestr is a utility tool for string literals in CUE files.'
-  version '0.4.1'
+  version '0.4.2'
   homepage 'https://github.com/k1LoW/cuestr'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.1/cuestr_v0.4.1_darwin_arm64.zip'
-      sha256 '40504d53d612a9c2e718a6b6f2455225f067e65cf13d8b0a9b1588e2df99744f'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.2/cuestr_v0.4.2_darwin_arm64.zip'
+      sha256 'd9f294c571ee14de4241bd35925c085e1dabab838d6e2211a8598f0e30c97afa'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.1/cuestr_v0.4.1_darwin_amd64.zip'
-      sha256 '0b8fef93cd2ba2208642c3c3cc294ebf38d0bec905ea66bb0a111eadefa090ab'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.2/cuestr_v0.4.2_darwin_amd64.zip'
+      sha256 'aa35c56350a0a46f3673e0c24e3356f84697a89ca9e9977851ce12a8ab780aff'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.1/cuestr_v0.4.1_linux_arm64.tar.gz'
-      sha256 'c56a41155189832796f8bf54474cce1fc9152c02e20320361c6fc02f4ca8cad6'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.2/cuestr_v0.4.2_linux_arm64.tar.gz'
+      sha256 'b901f0c6baba2e678f7031a0a940c70ab298f94ec0ff9c534e6d450d8c7c6b01'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.1/cuestr_v0.4.1_linux_amd64.tar.gz'
-      sha256 'c4b71e0f366177dab44d00d56d5769ddb8ccc82ab21b91ecedbbed8974bcd171'
+      url 'https://github.com/k1LoW/cuestr/releases/download/v0.4.2/cuestr_v0.4.2_linux_amd64.tar.gz'
+      sha256 'a92491d26ee57fd79c5de73b5b5e27bc830f19deafddb67614996635ec063fa9'
     end
   end
 
