0e84a4c3eaa3811ab6c561ccdaf25fb6c0de884d
k1LoW <k1LoW@users.noreply.github.com>
2025-12-23 06:05:44 +0000

    Commit from GitHub Actions (Update formulae)

--- gostyle.rb
+++ gostyle.rb
@@ -1,26 +1,26 @@
 class Gostyle < Formula
-  version '0.25.1'
+  version '0.25.2'
   homepage 'https://github.com/k1LoW/gostyle'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.1/gostyle_v0.25.1_darwin_arm64.zip'
-      sha256 'a688d88b5cf18d8af25fbf7f1f27f358f6e1e15f6936d18388318346775fee9c'
+      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.2/gostyle_v0.25.2_darwin_arm64.zip'
+      sha256 '5d39fd08c7960c1f334bbb7ae1c160deb07494cd81287ed9fa8890e8f83f16b1'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.1/gostyle_v0.25.1_darwin_amd64.zip'
-      sha256 '796096cdc5074fdb745fc8ef0de4d1f1586ee42888c3202203a3f293d99eeb86'
+      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.2/gostyle_v0.25.2_darwin_amd64.zip'
+      sha256 '82a3ce6957c234646821387e9e141fd5683fd101ed17619960955ee0a2e9c1c0'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.1/gostyle_v0.25.1_linux_arm64.tar.gz'
-      sha256 '998b3967fba1c761dfeab54fca78cb76053bc513abef528feea8f191573e86af'
+      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.2/gostyle_v0.25.2_linux_arm64.tar.gz'
+      sha256 '96e83be7d93b4d87f921e5be421d9107a010716c524d50bdb23f939af6d807a9'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.1/gostyle_v0.25.1_linux_amd64.tar.gz'
-      sha256 'ad9787192ef647b263a2ce69190c4bf6dfd026375f6a5495948f3329cd6815aa'
+      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.2/gostyle_v0.25.2_linux_amd64.tar.gz'
+      sha256 'a364011ee27111185f030f0b3e5830b3e292026c73d2b08ad0ecbdbae48ef0d0'
     end
   end
 
--- tbls.rb
+++ tbls.rb
@@ -1,29 +1,29 @@
 class Tbls < Formula
   desc 'tbls is a CI-Friendly tool for document a database, written in Go.'
-  version '1.92.1'
+  version '1.92.2'
   homepage 'https://github.com/k1LoW/tbls'
 
   deprecate! date: "2025-08-02", because: "please use the official Homebrew formula instead"
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.92.1/tbls_v1.92.1_darwin_arm64.zip'
-      sha256 'b2243cc4bf7d00ac01f3e96eaee0de51529ce3f6b414c41604ee676001d8c06e'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.92.2/tbls_v1.92.2_darwin_arm64.zip'
+      sha256 '76b6bc7174dd06086da00a04eda83a47b4f07f2555609f70b1fed483b8a7eceb'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.92.1/tbls_v1.92.1_darwin_amd64.zip'
-      sha256 'd932c73a293246b407b2fcebf05190020404e383f17a0445cebc349b0b08ad18'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.92.2/tbls_v1.92.2_darwin_amd64.zip'
+      sha256 '70addacb2ede1a830a473063a1a6934ce589578fd3a1759e436f927d2a86fbab'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.92.1/tbls_v1.92.1_linux_arm64.tar.gz'
-      sha256 'b652b3dc70eb40983f69e35d13c377fb83b38018c080eed00f229fcefc4a3b59'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.92.2/tbls_v1.92.2_linux_arm64.tar.gz'
+      sha256 'a8a998d2dabe71e9dfba97281614c991ae1c4e8710a140a26e056ad834d49f81'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tbls/releases/download/v1.92.1/tbls_v1.92.1_linux_amd64.tar.gz'
-      sha256 'a488568c0da443d35bcf3c994f61fa837197b85994448bf949f8e01066a8bf22'
+      url 'https://github.com/k1LoW/tbls/releases/download/v1.92.2/tbls_v1.92.2_linux_amd64.tar.gz'
+      sha256 'caf706859d582134f6e14dc4e61a0e533edb96ffa8f27d15b958b5898bef1256'
     end
   end
 
