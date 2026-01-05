675eb7a010227e7cfd026acd1b909076bd694314
k1LoW <k1LoW@users.noreply.github.com>
2026-01-05 15:30:17 +0000

    Commit from GitHub Actions (Update formulae)

--- git-wt.rb
+++ git-wt.rb
@@ -1,27 +1,27 @@
 class GitWt < Formula
   desc 'A Git subcommand that makes `git worktree` simple'
-  version '0.8.2'
+  version '0.9.0'
   homepage 'https://github.com/k1LoW/git-wt'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.8.2/git-wt_v0.8.2_darwin_arm64.zip'
-      sha256 '86f261e60172d2ebd2cfc14bfea38ca98bf55d8bae29f47d96163b0bae67c131'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.9.0/git-wt_v0.9.0_darwin_arm64.zip'
+      sha256 '3f92335ab789245faa0f83d3a2ecdb613aafeefc01f561fe06a8d98aebab4415'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.8.2/git-wt_v0.8.2_darwin_amd64.zip'
-      sha256 '24c92a291098819c9309d3929471e99315e501a84201e3d6c25130ac01a3f87a'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.9.0/git-wt_v0.9.0_darwin_amd64.zip'
+      sha256 '22d0cd3355b4f8e93fb12e3aa4ddf147052fc78ece6fecd22cc41b3c6518fa95'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.8.2/git-wt_v0.8.2_linux_arm64.tar.gz'
-      sha256 '4bdda0f5e9dde27dbcb91c80f743abd617cf912fba21c15021d12b1d864c052c'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.9.0/git-wt_v0.9.0_linux_arm64.tar.gz'
+      sha256 '796ccbac9d5345ee14e3d550afe0ced052fec2f7e2cf3a8e20504ee349a93f3e'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.8.2/git-wt_v0.8.2_linux_amd64.tar.gz'
-      sha256 '35df02482d22c795a2b14f4c855d6b75ef7813289612e39047737599c0c442fd'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.9.0/git-wt_v0.9.0_linux_amd64.tar.gz'
+      sha256 '23d99b17d37dc1231e90280ecd56c5e8977caf8cc0f69c81e6fbc1282f601118'
     end
   end
 
