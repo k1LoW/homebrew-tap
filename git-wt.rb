3e6ac91b08f6d85a01952ff3d54a1ce531c188e4
k1LoW <k1LoW@users.noreply.github.com>
2026-01-06 12:33:30 +0000

    Commit from GitHub Actions (Update formulae)

--- git-wt.rb
+++ git-wt.rb
@@ -1,27 +1,27 @@
 class GitWt < Formula
   desc 'A Git subcommand that makes `git worktree` simple'
-  version '0.10.0'
+  version '0.11.0'
   homepage 'https://github.com/k1LoW/git-wt'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.10.0/git-wt_v0.10.0_darwin_arm64.zip'
-      sha256 'd2f287e6587f94c685d7388e8bf6b5200ff4bcb7e55edb3872dca52711dc003c'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.11.0/git-wt_v0.11.0_darwin_arm64.zip'
+      sha256 '355668acb7fca8027437890c0885bec568eb425913db6f5ec2295bd3bcb68703'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.10.0/git-wt_v0.10.0_darwin_amd64.zip'
-      sha256 '5e24992462d68aea1e19c017de80adfa3de7cf3f3b0e33a4aa72ad608f0a23a4'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.11.0/git-wt_v0.11.0_darwin_amd64.zip'
+      sha256 'bf6a28fe68c908a03aee31b43d86aa2881a2c05008873a56957085a81772d1a8'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.10.0/git-wt_v0.10.0_linux_arm64.tar.gz'
-      sha256 '1a847cc82fa57742f05bfa6bb3e09b5749c94680a6907235484c84324763b7ed'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.11.0/git-wt_v0.11.0_linux_arm64.tar.gz'
+      sha256 'c1c29795850be32b93245408fa1eea6c6d02da69a75b4bd889662fb2576a96ba'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.10.0/git-wt_v0.10.0_linux_amd64.tar.gz'
-      sha256 'a969f3f349a36a5629054c0a617c1f2f128361565e07631cd438a7cfb4cfd2b7'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.11.0/git-wt_v0.11.0_linux_amd64.tar.gz'
+      sha256 '443a53425ac5bdfcf64e5be707a1f0bc2df5032ece5ca8ba7f7688d69f507ed3'
     end
   end
 
