1ece9123aacc79cb42a1e857a4148f6f69cf80d8
k1LoW <k1LoW@users.noreply.github.com>
2026-01-20 10:39:29 +0000

    Commit from GitHub Actions (Update formulae)

--- git-wt.rb
+++ git-wt.rb
@@ -1,27 +1,27 @@
 class GitWt < Formula
   desc 'A Git subcommand that makes `git worktree` simple'
-  version '0.13.1'
+  version '0.14.0'
   homepage 'https://github.com/k1LoW/git-wt'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.13.1/git-wt_v0.13.1_darwin_arm64.zip'
-      sha256 'd18c80c36c7821e26b99d839c472625e58e77f31a1d8299006aaec31c817b02d'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.14.0/git-wt_v0.14.0_darwin_arm64.zip'
+      sha256 '813cd745796389ffe4590639692c13e161f7ae490807915bb5dfec2708154a95'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.13.1/git-wt_v0.13.1_darwin_amd64.zip'
-      sha256 'f2382f8076c0079dbbe0b9e5d683d2495104782e0cd28f5e7cfbbf1c12f91b66'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.14.0/git-wt_v0.14.0_darwin_amd64.zip'
+      sha256 '8db1cb96bc240c1b70468dc3f77013fbc294f4c6f6cb1f94c8800fa75a039d94'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.13.1/git-wt_v0.13.1_linux_arm64.tar.gz'
-      sha256 '905b597ee27c62720a80ff8300e27e05d36f44d5e6efabd912525c67b1ed51fb'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.14.0/git-wt_v0.14.0_linux_arm64.tar.gz'
+      sha256 'a3674cd0f329ae4e3fc2279036ea5a0e333bf5840f4e27768d6a942835994a8a'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.13.1/git-wt_v0.13.1_linux_amd64.tar.gz'
-      sha256 'd0faee5b9f1c13d7486e5d1e5b14237e5a6415e317b9a70d7da26dbd83d0eb80'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.14.0/git-wt_v0.14.0_linux_amd64.tar.gz'
+      sha256 '7e93121a18426ce30ca4d4e66c0cbac054f3d91550ffc9fd79e73d6b895d1b3e'
     end
   end
 
