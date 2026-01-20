5d911d8249805ba9baaf1d3f486e33e772276664
k1LoW <k1LoW@users.noreply.github.com>
2026-01-20 12:32:50 +0000

    Commit from GitHub Actions (Update formulae)

--- git-wt.rb
+++ git-wt.rb
@@ -1,27 +1,27 @@
 class GitWt < Formula
   desc 'A Git subcommand that makes `git worktree` simple'
-  version '0.14.1'
+  version '0.14.2'
   homepage 'https://github.com/k1LoW/git-wt'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.14.1/git-wt_v0.14.1_darwin_arm64.zip'
-      sha256 '8757425de5527b075be93e4443b1a57cd218492120fcfbbe5dc4510748e25cf1'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.14.2/git-wt_v0.14.2_darwin_arm64.zip'
+      sha256 '8174ebc140693103c964b4cd992b277012f2307eb72b01da72d576234ac20ca9'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.14.1/git-wt_v0.14.1_darwin_amd64.zip'
-      sha256 '3390db4d8b641079a67f9c4fc8505b540bbbad6ed25645adecaa79a6527bd0c8'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.14.2/git-wt_v0.14.2_darwin_amd64.zip'
+      sha256 'f62f63fbfbca444be5c7c55676a534b705a5add1e80bd877031bb55379f28c27'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.14.1/git-wt_v0.14.1_linux_arm64.tar.gz'
-      sha256 'dbf8b2620fad2c8d42b6dd1641a82936b292362fdb294997373fb1a4365ff9ac'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.14.2/git-wt_v0.14.2_linux_arm64.tar.gz'
+      sha256 '93a5de5704f81352820d09a4bdc147102637b6165bb9d3c0b72cca6d9907709a'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.14.1/git-wt_v0.14.1_linux_amd64.tar.gz'
-      sha256 'c2bfc52d36eaa775dfdc8b578e534229e3ba2fc06e22e25839b776f3746ab81c'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.14.2/git-wt_v0.14.2_linux_amd64.tar.gz'
+      sha256 '5af2a8a839dccd60b7e25a1d8e00de9574213a0a4264b636a7f773278e6c7faf'
     end
   end
 
