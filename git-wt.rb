f0c563bea055c7987b497756dc2a72e62c869145
k1LoW <k1LoW@users.noreply.github.com>
2026-01-27 04:54:10 +0000

    Commit from GitHub Actions (Update formulae)

--- git-wt.rb
+++ git-wt.rb
@@ -1,27 +1,27 @@
 class GitWt < Formula
   desc 'A Git subcommand that makes `git worktree` simple'
-  version '0.15.1'
+  version '0.16.0'
   homepage 'https://github.com/k1LoW/git-wt'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.15.1/git-wt_v0.15.1_darwin_arm64.zip'
-      sha256 'd0dd5af5f8c5314065b1ba00bba652734b99668c8e662aa7ba8ae1464bfecd79'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.16.0/git-wt_v0.16.0_darwin_arm64.zip'
+      sha256 '2000755e756326e26202be624f2cc5ce8bd991e5f89b536454a22b2d8dfb7ff7'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.15.1/git-wt_v0.15.1_darwin_amd64.zip'
-      sha256 '9079cf937191173d3131eab19f69b9514a2403278d33e0e524f40ce79d90591b'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.16.0/git-wt_v0.16.0_darwin_amd64.zip'
+      sha256 '81c044f1089217a7bb53db68b91dfb8baf928318cabf59431fdfa08290f33344'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.15.1/git-wt_v0.15.1_linux_arm64.tar.gz'
-      sha256 'dded48a826ca311194bd92bd58a0ea9adec281b347105c56cc317d5a267d555b'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.16.0/git-wt_v0.16.0_linux_arm64.tar.gz'
+      sha256 '8dfa72753ca9f68591860e428d7ad569ecd2c163df0105793def014943d72d75'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.15.1/git-wt_v0.15.1_linux_amd64.tar.gz'
-      sha256 '18ac6f62a69f2a5bb59a037074cfa0cdfa1924f2098890af2add64b0f6c2b76b'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.16.0/git-wt_v0.16.0_linux_amd64.tar.gz'
+      sha256 '7d13d37733ae1eea6064baa7cc932f63603ca01c275fd21ec6bf1a12cf99fa59'
     end
   end
 
