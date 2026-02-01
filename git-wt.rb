09bb32b62d6b2f0b0bb20004afa5b03aed1e9db1
k1LoW <k1LoW@users.noreply.github.com>
2026-02-01 18:06:22 +0000

    Commit from GitHub Actions (Update formulae)

--- git-wt.rb
+++ git-wt.rb
@@ -1,27 +1,27 @@
 class GitWt < Formula
   desc 'A Git subcommand that makes `git worktree` simple'
-  version '0.16.1'
+  version '0.17.0'
   homepage 'https://github.com/k1LoW/git-wt'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.16.1/git-wt_v0.16.1_darwin_arm64.zip'
-      sha256 'c67ea11b1d797f297b2c9a709a4b61bf08c0feddb2690307531af3a834929b1e'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.17.0/git-wt_v0.17.0_darwin_arm64.zip'
+      sha256 'e69e8a893a4e7e0da328163666db4f9546f258a6cab353bef7a932efe1fda8a0'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.16.1/git-wt_v0.16.1_darwin_amd64.zip'
-      sha256 'dd89da7626b7f4335254febf428b6af5e52dfdf1260f3883ef86d9790d0e1f9c'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.17.0/git-wt_v0.17.0_darwin_amd64.zip'
+      sha256 '54608faa973b915a7f896da89c55afc4b27843a8623065fbc1b9ffcddd3c5438'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.16.1/git-wt_v0.16.1_linux_arm64.tar.gz'
-      sha256 '6b344e7ffc0136419219913ffd6a03703440f3fdcfce6e90b5829641977061c9'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.17.0/git-wt_v0.17.0_linux_arm64.tar.gz'
+      sha256 '37eee880929bd356f88d134c394ccc2a748882389ff81ec94b002d3c443fad4d'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.16.1/git-wt_v0.16.1_linux_amd64.tar.gz'
-      sha256 'e9891fdaeaac3b78c6fb9e81142237f2583d95da16e153bccc3414a94536a8ce'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.17.0/git-wt_v0.17.0_linux_amd64.tar.gz'
+      sha256 'dc56818ddb1bd13f9ddfb7f59a2e5a3b1244cf4d0bf9ffc1ddec6338c38a4685'
     end
   end
 
--- gostyle.rb
+++ gostyle.rb
@@ -1,26 +1,26 @@
 class Gostyle < Formula
-  version '0.25.2'
+  version '0.25.3'
   homepage 'https://github.com/k1LoW/gostyle'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.2/gostyle_v0.25.2_darwin_arm64.zip'
-      sha256 '5d39fd08c7960c1f334bbb7ae1c160deb07494cd81287ed9fa8890e8f83f16b1'
+      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.3/gostyle_v0.25.3_darwin_arm64.zip'
+      sha256 '74ff1f4a70c22cdc00009acb78739a546085bb9ee6c1fe7aeff93e3ca213d37a'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.2/gostyle_v0.25.2_darwin_amd64.zip'
-      sha256 '82a3ce6957c234646821387e9e141fd5683fd101ed17619960955ee0a2e9c1c0'
+      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.3/gostyle_v0.25.3_darwin_amd64.zip'
+      sha256 '076532ca457ac9650bf660c1ae8fb921ef591ff70fdce02c04fc6247817c58af'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.2/gostyle_v0.25.2_linux_arm64.tar.gz'
-      sha256 '96e83be7d93b4d87f921e5be421d9107a010716c524d50bdb23f939af6d807a9'
+      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.3/gostyle_v0.25.3_linux_arm64.tar.gz'
+      sha256 '014efb474a0317f5d51cbff21ad75776bb57bc44c465409f3f83aa76451f6054'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.2/gostyle_v0.25.2_linux_amd64.tar.gz'
-      sha256 'a364011ee27111185f030f0b3e5830b3e292026c73d2b08ad0ecbdbae48ef0d0'
+      url 'https://github.com/k1LoW/gostyle/releases/download/v0.25.3/gostyle_v0.25.3_linux_amd64.tar.gz'
+      sha256 '0745b1ba8a2b5175506cc9a7f9b5e9dd18e6de7c56c584b054500f3d4bca39f5'
     end
   end
 
