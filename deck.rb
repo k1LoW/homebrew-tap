412bf8fe3ada19db53263173dc9a30ab17402a1a
k1LoW <k1LoW@users.noreply.github.com>
2026-02-02 18:11:51 +0000

    Commit from GitHub Actions (Update formulae)

--- deck.rb
+++ deck.rb
@@ -1,29 +1,29 @@
 class Deck < Formula
   desc 'deck is a tool for creating deck using Google Slides.'
-  version '1.22.1'
+  version '1.23.0'
   homepage 'https://github.com/k1LoW/deck'
 
   deprecate! date: "2025-08-02", because: "please use the official Homebrew formula instead"
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/deck/releases/download/v1.22.1/deck_v1.22.1_darwin_arm64.zip'
-      sha256 'f89ff2522a7a2fc2fa2de40f04a4ffaf5e2b76464be741d03e892bc8cc74b106'
+      url 'https://github.com/k1LoW/deck/releases/download/v1.23.0/deck_v1.23.0_darwin_arm64.zip'
+      sha256 '51c289e25c1dc8d8be87a6cc6f210986c762cc8ff1d5c414ea61356d3ade17a2'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/deck/releases/download/v1.22.1/deck_v1.22.1_darwin_amd64.zip'
-      sha256 '611f08e22a01a96f1c53e1f8cab44e8ffa751a054155cb93aa992b1a50c7ce54'
+      url 'https://github.com/k1LoW/deck/releases/download/v1.23.0/deck_v1.23.0_darwin_amd64.zip'
+      sha256 '29c714cfdaeb3345f4cb20831cd35f3ffec86c646ef894799c2ee7d693682791'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/deck/releases/download/v1.22.1/deck_v1.22.1_linux_arm64.tar.gz'
-      sha256 'aede00897cabc4665afed18fb185babd2927be3698a4ed7cdeba81b2e4f352ef'
+      url 'https://github.com/k1LoW/deck/releases/download/v1.23.0/deck_v1.23.0_linux_arm64.tar.gz'
+      sha256 '7414531b11a09044ad1bd9f2a4c9bf5d2641f908ae92dcfbbfcee4259c6f487c'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/deck/releases/download/v1.22.1/deck_v1.22.1_linux_amd64.tar.gz'
-      sha256 'd3c314ba7d77b76ba6c33ac8c3468204c0a265cf87a1d7962bf44c1d4880374c'
+      url 'https://github.com/k1LoW/deck/releases/download/v1.23.0/deck_v1.23.0_linux_amd64.tar.gz'
+      sha256 '35f6099f496aef3070d8aafa72639461d186a099707c2ce00171414aba7fdb1f'
     end
   end
 
--- octocov.rb
+++ octocov.rb
@@ -1,24 +1,24 @@
 class Octocov < Formula
   desc 'octocov is a toolkit for collecting code metrics (code coverage, code to test ratio and test execution time).'
-  version '0.74.2'
+  version '0.74.3'
   license "MIT"
   homepage 'https://github.com/k1LoW/octocov'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.74.2/octocov_v0.74.2_darwin_arm64.zip'
-      sha256 '20a6cf6826da90948283a5758dcc59d1133e7cca756707aa07aa541db9a973c3'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.74.3/octocov_v0.74.3_darwin_arm64.zip'
+      sha256 'cc914c59bdf5271c2319abf1b77b3743905f8cf686054d992b71fe66e202cb75'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.74.2/octocov_v0.74.2_darwin_amd64.zip'
-      sha256 '194a19635e36a1508dc0e51c1adfbbfc1e960b6511e75eb0b0c37979e096228f'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.74.3/octocov_v0.74.3_darwin_amd64.zip'
+      sha256 '91b107c056d9650aaa83377fab10b8201ad728e17425c7edf07dcbd2be619986'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.74.2/octocov_v0.74.2_linux_amd64.tar.gz'
-      sha256 '8f407017d6f776c2472ff70a95408804c4bb7b8fbe112ef6223b669c9012f6ff'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.74.3/octocov_v0.74.3_linux_amd64.tar.gz'
+      sha256 '510dd447f58f268043a1d6c9d73af89d3b7391f26a865c859b4389cfa9482d12'
     end
   end
 
