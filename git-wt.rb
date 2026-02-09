d5a2f14ebf55167eae79e23c454d196addd6498d
k1LoW <k1LoW@users.noreply.github.com>
2026-02-09 15:01:32 +0000

    Commit from GitHub Actions (Update formulae)

--- git-wt.rb
+++ git-wt.rb
@@ -1,27 +1,27 @@
 class GitWt < Formula
   desc 'A Git subcommand that makes `git worktree` simple'
-  version '0.19.1'
+  version '0.20.0'
   homepage 'https://github.com/k1LoW/git-wt'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.19.1/git-wt_v0.19.1_darwin_arm64.zip'
-      sha256 'e975b1d9033c634f537d65036b14361265e5bc039118bbed890c858ea6061e56'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.20.0/git-wt_v0.20.0_darwin_arm64.zip'
+      sha256 '364d04fd1dcc54ff0e5b91ef75ebbf94a01bdb2dfb426b565d5cfef34a83a943'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.19.1/git-wt_v0.19.1_darwin_amd64.zip'
-      sha256 'ebc34bf68887e9cb7dacb9cae4489cf8d6519319c70e65e175443b491849cc7b'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.20.0/git-wt_v0.20.0_darwin_amd64.zip'
+      sha256 '10f6c98833583be32f3a073bd4f68362b459da215a0941e897311edc29687627'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.19.1/git-wt_v0.19.1_linux_arm64.tar.gz'
-      sha256 'aa5b52a92825184983e87cbbf3ed04880e0e054e15f35709d2781313cb318ecb'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.20.0/git-wt_v0.20.0_linux_arm64.tar.gz'
+      sha256 '6ffb9e34597fd6e95c30b9c38424a3df60779a036132b772cdc1fc0ee58af556'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.19.1/git-wt_v0.19.1_linux_amd64.tar.gz'
-      sha256 '931f215e43f6d5e951ee801e0a069920979feb9689c92d95f057b2638f09be1a'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.20.0/git-wt_v0.20.0_linux_amd64.tar.gz'
+      sha256 '8252b2781ee5b448d0c341db9cd7065275e5ea0e7a0c104c6518110b136c4ab2'
     end
   end
 
