9a3ee06d343d37655481d0c6120cb878fd42e2e2
k1LoW <k1LoW@users.noreply.github.com>
2026-02-16 02:03:45 +0000

    Commit from GitHub Actions (Update formulae)

--- git-wt.rb
+++ git-wt.rb
@@ -1,27 +1,27 @@
 class GitWt < Formula
   desc 'A Git subcommand that makes `git worktree` simple'
-  version '0.21.1'
+  version '0.22.0'
   homepage 'https://github.com/k1LoW/git-wt'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.21.1/git-wt_v0.21.1_darwin_arm64.zip'
-      sha256 '4a268ca2a99d5c424105828f481bb8eab57433336b6e736b792e824658c33aca'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.22.0/git-wt_v0.22.0_darwin_arm64.zip'
+      sha256 '560333af08cd6ba503fd19a4b79f306c9e0805fa46eebead7470c222538de526'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.21.1/git-wt_v0.21.1_darwin_amd64.zip'
-      sha256 '77596cca46e91f6f2bff0adb3cd17306cce177fc289fb33400d527151ef1e8a0'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.22.0/git-wt_v0.22.0_darwin_amd64.zip'
+      sha256 '8783131a0173c16b5da3e09ec15a2306c2e3f586ad87248f2431e4c694dfcbbc'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.21.1/git-wt_v0.21.1_linux_arm64.tar.gz'
-      sha256 'f58f48d8745e8aad1d26c8b496599bef1543f6c54cb4d15acb6b6237d02193df'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.22.0/git-wt_v0.22.0_linux_arm64.tar.gz'
+      sha256 '2d823dd32844860df8928211e6b80317063c5d34fb6e8777792d5a710c3e4257'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.21.1/git-wt_v0.21.1_linux_amd64.tar.gz'
-      sha256 '9ac14bebbfaddee71935ca54f8969d0c47ab2cc050d9bf4c0e7c6139f3fdd88d'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.22.0/git-wt_v0.22.0_linux_amd64.tar.gz'
+      sha256 'c94e4f11cd60782de6e5c651ec3f81f9701ec1ca127741211b7346239f5bbfcd'
     end
   end
 
