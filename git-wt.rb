004fa0e53555a2ac78cc819d8ac095eee301339e
k1LoW <k1LoW@users.noreply.github.com>
2026-02-17 12:38:06 +0000

    Commit from GitHub Actions (Update formulae)

--- git-wt.rb
+++ git-wt.rb
@@ -1,27 +1,27 @@
 class GitWt < Formula
   desc 'A Git subcommand that makes `git worktree` simple'
-  version '0.23.0'
+  version '0.24.0'
   homepage 'https://github.com/k1LoW/git-wt'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.23.0/git-wt_v0.23.0_darwin_arm64.zip'
-      sha256 '6abe27b182a40ec9fe5d330f2f5550d615f99ee61055d7dc84d4d3ed43b89675'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.24.0/git-wt_v0.24.0_darwin_arm64.zip'
+      sha256 'e2d93b66bd24053ebd8c31eee5d0c52bac1b900aebcbcb8907a12b82980b703e'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.23.0/git-wt_v0.23.0_darwin_amd64.zip'
-      sha256 '72b1c798a099884b06434fa0eb20553e9fef86c09c960e5eb56343f69185b3ed'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.24.0/git-wt_v0.24.0_darwin_amd64.zip'
+      sha256 'cca6f4e9db63894b9ec8de4551b290d68bf73b475a1fac5f43f9d666d017ff62'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.23.0/git-wt_v0.23.0_linux_arm64.tar.gz'
-      sha256 '99b9c2ef5d543cf686eb85f5e7e2d38d92937a0efefd388e8df82a713c954e89'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.24.0/git-wt_v0.24.0_linux_arm64.tar.gz'
+      sha256 'bb1554b8806123e32bd354a70832b342b9a60ddbd5fcd2b0bbe4dabeaaf7ff33'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/git-wt/releases/download/v0.23.0/git-wt_v0.23.0_linux_amd64.tar.gz'
-      sha256 'ea26cfe207133a99695da4aa5dcbe271b6435ab227280817d3530d4b9afdd3d9'
+      url 'https://github.com/k1LoW/git-wt/releases/download/v0.24.0/git-wt_v0.24.0_linux_amd64.tar.gz'
+      sha256 'e0fbbabc4aa64c62a1ff24138c1a09d3f3c5b60bd5342f043c61aa24a5a0dbee'
     end
   end
 
