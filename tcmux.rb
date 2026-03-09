83f63c555b6c3f964956fdd0257dd7039975ab98
k1LoW <k1LoW@users.noreply.github.com>
2026-03-09 22:56:03 +0000

    Commit from GitHub Actions (Update formulae)

--- tcmux.rb
+++ tcmux.rb
@@ -1,27 +1,27 @@
 class Tcmux < Formula
   desc 'tcmux is a terminal and Claude Code mux viewer.'
-  version '0.3.2'
+  version '0.4.0'
   homepage 'https://github.com/k1LoW/tcmux'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tcmux/releases/download/v0.3.2/tcmux_v0.3.2_darwin_arm64.zip'
-      sha256 'd0e455d73e7831e1a2b2ea089b048432446a958b73dba43a2bdac633e62045d8'
+      url 'https://github.com/k1LoW/tcmux/releases/download/v0.4.0/tcmux_v0.4.0_darwin_arm64.zip'
+      sha256 'd386ed5d1c5fed173ed224b6ea2ce86ffa8029a810cf67a9797d017c0e1745e5'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tcmux/releases/download/v0.3.2/tcmux_v0.3.2_darwin_amd64.zip'
-      sha256 'bec859859ddae6b518e5ad9091ca74ef85c31d7ab6ecf0f7d5648bbda81f2203'
+      url 'https://github.com/k1LoW/tcmux/releases/download/v0.4.0/tcmux_v0.4.0_darwin_amd64.zip'
+      sha256 'e04df7999d47d3f8056157f0235c7ac7521d24a3cb3a29412dd54f80b7256251'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tcmux/releases/download/v0.3.2/tcmux_v0.3.2_linux_arm64.tar.gz'
-      sha256 '6688333761609d1f5848122332fae6956cc33d70f589e6afc2db1b663413d9f7'
+      url 'https://github.com/k1LoW/tcmux/releases/download/v0.4.0/tcmux_v0.4.0_linux_arm64.tar.gz'
+      sha256 '14f3aea409a445cc6a11a7900d17a59f6f7b0f39f7c1402b73fa50536167a754'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tcmux/releases/download/v0.3.2/tcmux_v0.3.2_linux_amd64.tar.gz'
-      sha256 '421ddeaff1b0f6ed40e43da6d2019a5d97b38a5b0e60877e91c50e8091014bd7'
+      url 'https://github.com/k1LoW/tcmux/releases/download/v0.4.0/tcmux_v0.4.0_linux_amd64.tar.gz'
+      sha256 '9870f9939818c503b1d679eb6731fe113544ed0164f5328f7f57883a1f1dcbb4'
     end
   end
 
