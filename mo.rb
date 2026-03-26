4426352328395068be41ea5b5ea025e3551f9839
k1LoW <k1LoW@users.noreply.github.com>
2026-03-26 09:07:44 +0000

    Commit from GitHub Actions (Update formulae)

--- mo.rb
+++ mo.rb
@@ -1,27 +1,27 @@
 class Mo < Formula
   desc 'mo is a Markdown viewer that opens .md files in a browser.'
-  version '0.21.0'
+  version '0.22.0'
   homepage 'https://github.com/k1LoW/mo'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/mo/releases/download/v0.21.0/mo_v0.21.0_darwin_arm64.zip'
-      sha256 '296ac4d471f08558852bb9a61e468a75c7ca356cdc7dad705babce51074845d3'
+      url 'https://github.com/k1LoW/mo/releases/download/v0.22.0/mo_v0.22.0_darwin_arm64.zip'
+      sha256 'd829dee72563a636da6942ffed216ea1725c0f8f6bac4490219485d7a549b020'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/mo/releases/download/v0.21.0/mo_v0.21.0_darwin_amd64.zip'
-      sha256 'ef9ad28383a3ba9e9dc69d7ef122c0ac0b40203bb3b133a69ad6966c1b449cce'
+      url 'https://github.com/k1LoW/mo/releases/download/v0.22.0/mo_v0.22.0_darwin_amd64.zip'
+      sha256 'c5cc560f3e66edfd0a311882ea0604887dfee83777f4a1c5df4da35ea7515c02'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/mo/releases/download/v0.21.0/mo_v0.21.0_linux_arm64.tar.gz'
-      sha256 '19f2db55a624b8a210096770c22a0bf5f7245ab0194832efaf5c85582361aca7'
+      url 'https://github.com/k1LoW/mo/releases/download/v0.22.0/mo_v0.22.0_linux_arm64.tar.gz'
+      sha256 'b7cf79f0bb812619e6a9b180863086103939a51154653c1fe904b84cf378ffa9'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/mo/releases/download/v0.21.0/mo_v0.21.0_linux_amd64.tar.gz'
-      sha256 '0c8800985e4d44a3910ca62bfe8a9afd51930f2366e7fc5f21708f34dece5d5c'
+      url 'https://github.com/k1LoW/mo/releases/download/v0.22.0/mo_v0.22.0_linux_amd64.tar.gz'
+      sha256 'dada4d9123fc2ba5829e212e97c5828fc8561eee151245a55d24b107d2aa2531'
     end
   end
 
