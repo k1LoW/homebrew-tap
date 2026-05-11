1b4272bded9db95dd919e7774d0f6c9a9f49fa23
k1LoW <k1LoW@users.noreply.github.com>
2026-05-11 18:38:02 +0000

    Commit from GitHub Actions (Update formulae)

--- octocov-cachegrind.rb
+++ octocov-cachegrind.rb
@@ -1,27 +1,27 @@
 class OctocovCachegrind < Formula
   desc 'Generate octocov custom metrics JSON from the output of `valgrind --tool=cachegrind`.'
-  version '0.2.5'
+  version '0.2.6'
   homepage 'https://github.com/k1LoW/octocov-cachegrind'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.2.5/octocov-cachegrind_v0.2.5_darwin_arm64.zip'
-      sha256 'ddd056725d06cb6c42c68994adacfa29c032b2f07ea529a7374480d0f32a5bf0'
+      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.2.6/octocov-cachegrind_v0.2.6_darwin_arm64.zip'
+      sha256 'ee25f739f992ebb3c8f9ee73c650e60774fbe06fe9008c9c519e52365d6f9bd8'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.2.5/octocov-cachegrind_v0.2.5_darwin_amd64.zip'
-      sha256 '031e1a5ee88b44fa9544e11db21e21f4285a49b12b7f12258fd3961342ed49b9'
+      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.2.6/octocov-cachegrind_v0.2.6_darwin_amd64.zip'
+      sha256 '32719abd798d5a4fe58cabdaf0a09937640223e0a4f0c643123bad58cd9dda34'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.2.5/octocov-cachegrind_v0.2.5_linux_arm64.tar.gz'
-      sha256 '3800bfd1d43b4c9e61365709080f829cfc9ca90f144dd53d450a11a27e4d6eee'
+      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.2.6/octocov-cachegrind_v0.2.6_linux_arm64.tar.gz'
+      sha256 '2e9bfcfddf8210b6781f4cbb6003711f77de6f6ef50ef0c600538d4d56914dd5'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.2.5/octocov-cachegrind_v0.2.5_linux_amd64.tar.gz'
-      sha256 'ac10bd8669681d71429c815150df8c5d94c420403a17a825dcb51370d1416184'
+      url 'https://github.com/k1LoW/octocov-cachegrind/releases/download/v0.2.6/octocov-cachegrind_v0.2.6_linux_amd64.tar.gz'
+      sha256 'f8af8385438eaf4e80a92c49a67d3e00327d165c9e96b60d9cc8bf6bf926b071'
     end
   end
 
--- octocov-runn-coverage.rb
+++ octocov-runn-coverage.rb
@@ -1,27 +1,27 @@
 class OctocovRunnCoverage < Formula
   desc 'Generate octocov custom metrics JSON from the output of 'runn coverage'.'
-  version '0.1.10'
+  version '0.1.11'
   homepage 'https://github.com/k1LoW/octocov-runn-coverage'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.1.10/octocov-runn-coverage_v0.1.10_darwin_arm64.zip'
-      sha256 'b3cd0fc5ccf0ea007a27e79057748fb0b128cabccbc3dfa06e96f3b2f381c608'
+      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.1.11/octocov-runn-coverage_v0.1.11_darwin_arm64.zip'
+      sha256 '39ce8f8a6514f5e22dc7a6af68a3db4e623b352cc6b6d0b04f31592719f97bff'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.1.10/octocov-runn-coverage_v0.1.10_darwin_amd64.zip'
-      sha256 '7ae2cee43e03de8bd44d3a537fe139fd0266543c33241a169631fd5cfa23fce1'
+      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.1.11/octocov-runn-coverage_v0.1.11_darwin_amd64.zip'
+      sha256 'dd33de5904710a8853cdb323aae1773cd22e93c96cd567bd26635410b557aed3'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.1.10/octocov-runn-coverage_v0.1.10_linux_arm64.tar.gz'
-      sha256 '4ace081bbdedbe1bec31c243363119d8becd0544963c2c0042ced936582c9635'
+      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.1.11/octocov-runn-coverage_v0.1.11_linux_arm64.tar.gz'
+      sha256 'eb8a5a89892d517df87e8c075f4300117bf419b3c2f284c1e1030d432b70dd40'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.1.10/octocov-runn-coverage_v0.1.10_linux_amd64.tar.gz'
-      sha256 '8f6d0293508e16262b1ad934e031ea74f24dc53939cc5f84174e539765f25f09'
+      url 'https://github.com/k1LoW/octocov-runn-coverage/releases/download/v0.1.11/octocov-runn-coverage_v0.1.11_linux_amd64.tar.gz'
+      sha256 '276da6b0893dcdcc937400091855bae67a08e1d82efc5781fbb6f1c8f046fd57'
     end
   end
 
