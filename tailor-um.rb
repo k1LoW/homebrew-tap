9aaab69270812a9d41a9efdf83ab2c6c3b2e7579
k1LoW <k1LoW@users.noreply.github.com>
2026-05-15 18:33:35 +0000

    Commit from GitHub Actions (Update formulae)

--- tailor-um.rb
+++ tailor-um.rb
@@ -1,26 +1,26 @@
 class TailorUm < Formula
-  version '0.2.2'
+  version '0.3.0'
   homepage 'https://github.com/k1LoW/tailor-um'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tailor-um/releases/download/v0.2.2/tailor-um_v0.2.2_darwin_arm64.zip'
-      sha256 '53b16b8d60916daa97e051abd537d0445eb84072889a08abff09a3a774559d88'
+      url 'https://github.com/k1LoW/tailor-um/releases/download/v0.3.0/tailor-um_v0.3.0_darwin_arm64.zip'
+      sha256 '9a03ccfa49c6693b8de871d84a582a558cac4078c5b93d67b993b2b204e4bd00'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tailor-um/releases/download/v0.2.2/tailor-um_v0.2.2_darwin_amd64.zip'
-      sha256 'b3ffb8945132eb522991831377b8531005058af81382dd21f8cd52b8dc3b93ca'
+      url 'https://github.com/k1LoW/tailor-um/releases/download/v0.3.0/tailor-um_v0.3.0_darwin_amd64.zip'
+      sha256 'b8942dbd461bd4e2cecd8389dc8d5e37210e858c2333ad35340ed65099cb2595'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tailor-um/releases/download/v0.2.2/tailor-um_v0.2.2_linux_arm64.tar.gz'
-      sha256 '79cc45db2ef867e5fc56ad11b508c2941ae6f47e4f60f026040a762c1589529f'
+      url 'https://github.com/k1LoW/tailor-um/releases/download/v0.3.0/tailor-um_v0.3.0_linux_arm64.tar.gz'
+      sha256 'f180e31e28d0a73254a27e38182e1e7784359ee1c5229c6e5d8b15033d5fb3b3'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tailor-um/releases/download/v0.2.2/tailor-um_v0.2.2_linux_amd64.tar.gz'
-      sha256 '089cc75d6500eb754da61e09bb7a2e84ffd5d3d9e5785c94c0e4db2caff406a1'
+      url 'https://github.com/k1LoW/tailor-um/releases/download/v0.3.0/tailor-um_v0.3.0_linux_amd64.tar.gz'
+      sha256 '5a545c09c6b9ed29688c7b42e83db6eef91607139b0d21f8134631aa9823edc5'
     end
   end
 
