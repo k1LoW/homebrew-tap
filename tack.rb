f19200018a010233248903b48eb1c2796275e92d
k1LoW <k1LoW@users.noreply.github.com>
2026-06-09 06:58:50 +0000

    Commit from GitHub Actions (Update formulae)

--- tack.rb
+++ tack.rb
@@ -1,27 +1,27 @@
 class Tack < Formula
   desc 'tack is a one-shot deploy CLI for Tailor Platform Static Website Hosting'
-  version '0.2.0'
+  version '0.2.1'
   homepage 'https://github.com/k1LoW/tack'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/tack/releases/download/v0.2.0/tack_v0.2.0_darwin_arm64.zip'
-      sha256 '45b975cf44b1e4c1d162a7460c38e1135e2cb8c0de59fe8babc29e78df50fe51'
+      url 'https://github.com/k1LoW/tack/releases/download/v0.2.1/tack_v0.2.1_darwin_arm64.zip'
+      sha256 'a8a2c52de242004d4e8b408dcf327f55dbdd0a3461ee9fe49dcc3363a4607c72'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tack/releases/download/v0.2.0/tack_v0.2.0_darwin_amd64.zip'
-      sha256 '11ef3d0e22223f4095e4f187bde9bd5e777da835d3d9e44ef58636a0ec5b7980'
+      url 'https://github.com/k1LoW/tack/releases/download/v0.2.1/tack_v0.2.1_darwin_amd64.zip'
+      sha256 'a419f0a7eb2d691a679c7ddc9e7b217e93fa5a6327be3f084965f636765b6846'
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url 'https://github.com/k1LoW/tack/releases/download/v0.2.0/tack_v0.2.0_linux_arm64.tar.gz'
-      sha256 'ae178dde43ec20bf1a793bce433f2e58acee1b87045ab88f5bd1cd88ea7991a0'
+      url 'https://github.com/k1LoW/tack/releases/download/v0.2.1/tack_v0.2.1_linux_arm64.tar.gz'
+      sha256 '09e1baa31c6ae2ec756d8913e9f6b54ba1676bf5f911efabcaea31bbeab13f7f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/tack/releases/download/v0.2.0/tack_v0.2.0_linux_amd64.tar.gz'
-      sha256 'c352fb9f521054a7d536e21e214fdfc0e039718f28723cb38b0d80dfbebf6d17'
+      url 'https://github.com/k1LoW/tack/releases/download/v0.2.1/tack_v0.2.1_linux_amd64.tar.gz'
+      sha256 '283c57ed5af872432c94a992a9729c6e677235fa8cb39870488ca141522c0006'
     end
   end
 
