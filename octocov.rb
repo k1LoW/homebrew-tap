4190796f18b91324eee3f8b569237de8de39c3c2
k1LoW <k1LoW@users.noreply.github.com>
2026-09-21 18:10:04 +0000

    Commit from GitHub Actions (Update formulae)

--- octocov.rb
+++ octocov.rb
@@ -1,24 +1,24 @@
 class Octocov < Formula
   desc 'octocov is a toolkit for collecting code metrics (code coverage, code to test ratio and test execution time).'
-  version '0.80.0'
+  version '0.80.1'
   license "MIT"
   homepage 'https://github.com/k1LoW/octocov'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.80.0/octocov_v0.80.0_darwin_arm64.zip'
-      sha256 '13b2d6f06e828a1c04ae94b64f9bb4ac9a7a33053b672e454b5562ff4729f6e7'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.80.1/octocov_v0.80.1_darwin_arm64.zip'
+      sha256 'e5e57a9aa5f92de6f820e3ad53abfff36cd585cdfba51253974d4b1eaabfee8f'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.80.0/octocov_v0.80.0_darwin_amd64.zip'
-      sha256 '69436d1c94fd65503c31fc1e08d96df09b2d0d8bcb6cdf4b311c71fd907d8c24'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.80.1/octocov_v0.80.1_darwin_amd64.zip'
+      sha256 '17b8b199709c9d043d039f54ca70c22f1d824224112aec6b891f75f76f6bd43a'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/octocov/releases/download/v0.80.0/octocov_v0.80.0_linux_amd64.tar.gz'
-      sha256 'aaa7c95af41a303407ec248a73a6cf9d0c0e8843c14e30f215aac06c7e402b95'
+      url 'https://github.com/k1LoW/octocov/releases/download/v0.80.1/octocov_v0.80.1_linux_amd64.tar.gz'
+      sha256 '012c652747daef1631a3d2f5613ccb40347411b007049518f191d24c1d7bc850'
     end
   end
 
--- trivy-db-to.rb
+++ trivy-db-to.rb
@@ -1,23 +1,23 @@
 class TrivyDbTo < Formula
   desc 'trivy-db-to is a tool for migrating/converting vulnerability information from Trivy DB to other datasource.'
-  version '2.2.9'
+  version '2.2.10'
   homepage 'https://github.com/k1LoW/trivy-db-to'
 
   on_macos do
     if Hardware::CPU.arm?
-      url 'https://github.com/k1LoW/trivy-db-to/releases/download/v2.2.9/trivy-db-to_v2.2.9_darwin_arm64.zip'
-      sha256 '7d603fa2d9105531da8a7ac57d6533a4c43ed8e149c67de50835ceecedd5eb4e'
+      url 'https://github.com/k1LoW/trivy-db-to/releases/download/v2.2.10/trivy-db-to_v2.2.10_darwin_arm64.zip'
+      sha256 '2198f51b9228077cf80aa2e3c162322c3f8a0d363807012b575381d5754969dd'
     end
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/trivy-db-to/releases/download/v2.2.9/trivy-db-to_v2.2.9_darwin_amd64.zip'
-      sha256 '2fe8d647a8a626dcc8db49a69f7240424f123f4b37ae916ed38a9eebaa173651'
+      url 'https://github.com/k1LoW/trivy-db-to/releases/download/v2.2.10/trivy-db-to_v2.2.10_darwin_amd64.zip'
+      sha256 '4b211fbe91d77303caf329e3cbec7a229b3855a7b365fb3e8396282789fe0263'
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url 'https://github.com/k1LoW/trivy-db-to/releases/download/v2.2.9/trivy-db-to_v2.2.9_linux_amd64.tar.gz'
-      sha256 '35f0f65ae034404cfcdcc5a60d110c910d09556715dd2b427b92fef2eea89e84'
+      url 'https://github.com/k1LoW/trivy-db-to/releases/download/v2.2.10/trivy-db-to_v2.2.10_linux_amd64.tar.gz'
+      sha256 '7b897c3fc089c501e6d831caa45a9a14c02fcc056e0bf4d2c0261260b4781eec'
     end
   end
 
